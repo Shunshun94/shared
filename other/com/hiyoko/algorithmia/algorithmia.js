9e7202060a6f174044e8e090ec4e41af602f74af
Shunshun94 <shunshun9460@gmail.com>
2017-03-30 01:13:51 +0900

    Edit authentication

--- other/com/hiyoko/algorithmia/algorithmia.js
+++ other/com/hiyoko/algorithmia/algorithmia.js
@@ -1,14 +1,14 @@
 var com = com || {};
 com.hiyoko = com.hiyoko || {};
 com.hiyoko.Algorithmia = function(apiKey) {
-	Algorithmia.client(apiKey);
+	this.key = apiKey;
 };
 
 com.hiyoko.Algorithmia.prototype.request = function(url, params) {
 	var paramStr = (typeof params === 'object' ? JSON.stringify(params) : params);
 	
 	return new Promise(function(resolve, reject) {
-		Algorithmia.client.algo(url)
+		Algorithmia.client(this.key).algo(url)
 	    	.pipe(paramStr)
 	    	.then(function(output) {
 	    		if(output.error) {
