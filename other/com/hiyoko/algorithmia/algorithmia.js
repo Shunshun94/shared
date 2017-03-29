210d96c751a7eef65b53bc3c85768f42f5355c67
Shunshun94 <shunshun9460@gmail.com>
2017-03-30 01:09:02 +0900

    Add lacked sentence

--- other/com/hiyoko/algorithmia/algorithmia.js
+++ other/com/hiyoko/algorithmia/algorithmia.js
@@ -8,7 +8,7 @@ com.hiyoko.Algorithmia.prototype.request = function(url, params) {
 	var paramStr = (typeof params === 'object' ? JSON.stringify(params) : params);
 	
 	return new Promise(function(resolve, reject) {
-		client.algo(url)
+		Algorithmia.client.algo(url)
 	    	.pipe(paramStr)
 	    	.then(function(output) {
 	    		if(output.error) {
