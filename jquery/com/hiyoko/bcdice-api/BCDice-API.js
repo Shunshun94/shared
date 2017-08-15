e98cffa48530e368dde76ddac56e6a89acc6ab4e
Shunshun94 <shunshun9460@gmail.com>
2017-08-15 23:27:34 +0900

    Add dicer

---
+++ jquery/com/hiyoko/bcdice-api/BCDice-API.js
@@ -0,0 +1,31 @@
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.BCDiceAPIClient = function(url, opt_defaultSystem) {
+	this.url = url;
+	this.system = opt_defaultSystem || 'DiceBot';
+};
+
+com.hiyoko.BCDiceAPIClient.prototype.setSystem = function(opt_system) {
+	this.system = opt_system || 'DiceBot';
+	return this.system;
+};
+
+com.hiyoko.BCDiceAPIClient.prototype.sendRequest = function() {
+	return $.ajax();
+};
+
+
+com.hiyoko.BCDiceAPIClient.prototype.getVersion = function() {
+	
+};
+
+
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
+com.hiyoko.BCDiceAPIClient.prototype;
