46c475f7261d88df594532026205686519793e9b
Shunshun94 <shunshun9460@gmail.com>
2018-02-19 01:45:33 +0900

    initialize format is changed

--- jquery/io/github/shunshun94/scheduler/scheduler.js
+++ jquery/io/github/shunshun94/scheduler/scheduler.js
@@ -8,9 +8,10 @@ io.github.shunshun94.scheduler.Scheduler = class {
 		this.id = this.$html.attr('id') || 'io-github-shunshun94-scheduler-Scheduler';
 		this.timezone = opts.timezone ? (Number(opts.timezone) || 0) : 0;
 		this.appendable = opts.appendable ? Boolean(opts.appendable) : false;
-		this.startDate = opts.startDate || new Date();
 		this.dateFormat = opts.dateFormat || '%m/%d (%D)';
-		
+		this.initialSchedule = (opts.initialSchedule || io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE).sort((a, b) => {
+			return a.prepare - b.prepare;
+		});
 		this.buildComponents();
 		this.bindEvents();
 	}
@@ -37,9 +38,10 @@ io.github.shunshun94.scheduler.Scheduler = class {
 	
 	
 	buildComponents() {
-		const baseYear = this.startDate.getFullYear();
-		const baseMonth = this.startDate.getMonth();
-		const baseDay = this.startDate.getDate();
+		const startDate = new Date(this.initialSchedule[0].prepare) || new Date();
+		const baseYear = startDate.getFullYear();
+		const baseMonth = startDate.getMonth();
+		const baseDay = startDate.getDate();
 		
 		
 		for(var i = 0; i < io.github.shunshun94.scheduler.Scheduler.ONE_WEEK_DAYS ; i++) {
@@ -57,5 +59,42 @@ io.github.shunshun94.scheduler.Scheduler = class {
 };
 
 
+io.github.shunshun94.scheduler.Scheduler.generateSchedule = (
+	label,
+	startDate,
+	length = 540,
+	prepare = 0,
+	tidyUp = 0
+) => {
+	var result = {};
+	startDateNum = Number(startDate);
+	result.label = label;
+	result.start = startDateNum;
+	result.end = startDateNum + length * 60 * 1000;
+	result.prepare =  startDateNum - prepare * 60 * 1000;
+	result.tidyUp = result.end + tidyUp * 60 * 1000;
+	return result;
+};
+
 io.github.shunshun94.scheduler.Scheduler.ONE_WEEK_DAYS = 7;
 io.github.shunshun94.scheduler.Scheduler.DAYS = ['Sun.', 'Mon', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.'];
+io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE = new Date();
+io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES = {
+	YEAR: io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.getFullYear(),
+	MONTH: io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.getMonth(),
+	DATE: io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.getDate(),
+	HOUR: 9, MIN: 0
+};
+
+io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE = [0,1,2,3,4,5,6].map((diff) => {
+	return io.github.shunshun94.scheduler.Scheduler.generateSchedule(
+			`Schedule - ${diff + 1}`,
+			new Date(
+			io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES.YEAR,
+			io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES.MONTH,
+			io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES.DATE + diff,
+			io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES.HOUR,
+			io.github.shunshun94.scheduler.Scheduler.INITIAL_SCHEDULE_BASEDATE.VALUES.MIN),
+			540, 120, 30
+	);
+});
