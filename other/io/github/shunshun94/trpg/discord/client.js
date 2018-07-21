f786ddf758d99650be910791bfa662935f567632
Shunshun94 <shunshun9460@gmail.com>
2018-07-22 00:43:28 +0900

    多分直せた

--- other/io/github/shunshun94/trpg/discord/client.js
+++ other/io/github/shunshun94/trpg/discord/client.js
@@ -244,7 +244,7 @@ io.github.shunshun94.trpg.discord.Room = class extends io.github.shunshun94.trpg
 						result: 'OK',
 						characters: result.characters
 					});
-					//this.showInitTable(JSON.stringify(result.characters));
+					this.showInitTable(JSON.stringify(result.characters));
 				}, (error) => {
 					reject({
 						result: error, args: args
@@ -296,7 +296,7 @@ io.github.shunshun94.trpg.discord.Room = class extends io.github.shunshun94.trpg
 						result: 'OK',
 						characters: result.characters
 					});
-					//this.showInitTable(JSON.stringify(result.characters));
+					this.showInitTable(JSON.stringify(result.characters));
 				}, (error) => {
 					reject({
 						result: error
