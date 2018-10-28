6133a469426b9e679e27f1335382bde88e08726d
Shunshun94 <shunshun9460@gmail.com>
2018-10-28 20:41:56 +0900

    relationship is implemented but dicebot isn't edited

--- vue/io/github/shunshun94/trpg/discord/dicebots/diceBotEditor.js
+++ vue/io/github/shunshun94/trpg/discord/dicebots/diceBotEditor.js
@@ -1,3 +1,4 @@
+/*
 const diceBotEditor = new Vue({
 	el: '#dicebots-editor',
 	data: {
@@ -5,5 +6,22 @@ const diceBotEditor = new Vue({
 		command: '',
 		dice: '',
 		tableRaw: ''
+	}
+});*/
+
+const dicebot_editor = Vue.component('dicebot-editor', {
+	props: ['dicebot'],
+	template: `<div class="dicebots-editor" @change="edited">
+		<div class="dicebots-editor-header">
+			<span>ダイスボット名</span><input type="text" class="dicebots-editor-title" v-bind:value="dicebot.title" />
+			<span>コマンド</span><input type="text" class="dicebots-editor-command" v-bind:value="dicebot.command" />
+			<span>ダイス</span><input type="text" class="dicebots-editor-command" v-bind:value="dicebot.dice" />
+		</div>
+		<textarea class="dicebots-editor-tableRaw" v-bind:value="dicebot.tableRaw"></textarea>
+	</div>`,
+	methods: {
+		edited: function() {
+			this.$emit('dicebot-edited', this.dicebot);
+		}
 	}
 });
--- vue/io/github/shunshun94/trpg/discord/dicebots/dicebots.js
+++ vue/io/github/shunshun94/trpg/discord/dicebots/dicebots.js
@@ -1,20 +1,31 @@
 const savedDiceBots = localStorage.getItem('io-github-shunshun94-trpg-sample-discord-dicebot-dicebots');
+const dicebots = savedDiceBots ? JSON.parse(savedDiceBots) : [dicebotGenerator()];
 const diceBots = new Vue({
 	el: '#dicebots-list',
 	data: {
-		dicebots: savedDiceBots ? JSON.parse(savedDiceBots) : [dicebotGenerator()]
+		dicebots: dicebots,
+		dicebot: dicebots[0]
 	},
 	components: {
-		dicebot_item
+		dicebot_item, dicebot_editor
 	},
 	methods: {
 		selectBot: function(val) {
-			console.log(val);
+			dicebots.forEach((bot, i)=>{
+				if(bot.key === val.key) {
+					this.dicebot = this.dicebots[i];
+				}
+			});
+		},
+		editBot: function(val) {
+			 console.log(JSON.stringify(val, null, '  '));
 		},
 		addBot: function() {
+			const key = com.hiyoko.util.rndString(6);
 			this.dicebots.push({
+				key: key,
 				title: '新しいダイスボット',
-				command: `newDiceBot_${com.hiyoko.util.rndString(6)}`,
+				command: `newDiceBot_${key}`,
 				dice: '2d6',
 				tableRaw: [2,3,4,5,6,7,8,9,10,11,12].map((d)=>{return `${d}:出目${d}の時の結果`}).join('\n')
 			});
