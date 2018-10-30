63733075aac73e53d55b863788f133b2111baae2
shunshun94 <shunshun9460@gmail.com>
2018-10-30 18:41:10 +0900

    fix a bug

--- jquery/io/github/shunshun94/scheduler/scheduler.js
+++ jquery/io/github/shunshun94/scheduler/scheduler.js
@@ -436,13 +436,13 @@ io.github.shunshun94.scheduler.Scheduler = class {
 	
 	getCovereredSchedules(schedules, baseHead, baseTail) {
 		return schedules.filter((schedule, i)=>{
-			if(schedule.tidyUp > baseHead && schedule.tidyUp < baseTail) {
+			if(schedule.tidyUp >= baseHead && schedule.tidyUp <= baseTail) {
 				return true;
 			}
-			if(schedule.prepare > baseHead && schedule.prepare < baseTail) {
+			if(schedule.prepare >= baseHead && schedule.prepare <= baseTail) {
 				return true;
 			}
-			if(schedule.prepare < baseHead && schedule.tidyUp > baseTail) {
+			if(schedule.prepare <= baseHead && schedule.tidyUp >= baseTail) {
 				return true;
 			}
 			return false;
@@ -463,6 +463,7 @@ io.github.shunshun94.scheduler.Scheduler = class {
 				const baseTail = Number(new Date(baseYear, baseMonth, baseDay, 14, 30));
 				const schedules = this.getSchedulesByDate(date);
 				const coveredScheduels = this.getCovereredSchedules(schedules, baseHead, baseTail);
+				console.log(baseHead, baseTail, coveredScheduels);
 				if(coveredScheduels.length) {
 					return;
 				}
