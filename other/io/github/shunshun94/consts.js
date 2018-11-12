6f4d427e826b720f726ca8aa8350b77186f326a9
shunshun94 <shunshun9460@gmail.com>
2018-11-12 15:35:22 +0900

    Add util const files

---
+++ other/io/github/shunshun94/consts.js
@@ -0,0 +1,8 @@
+var io = io || {};
+io.github = io.github || {};
+io.github.shunshun94 = io.github.shunshun94 || {};
+io.github.shunshun94.CONSTS = io.github.shunshun94.CONSTS || {};
+
+io.github.shunshun94.CONSTS.EXCHANGE = {
+	USD_YEN: 115
+};
