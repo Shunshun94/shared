c0e9635504079df4510a753feb7c004328a6f020
Shunshun94 <shunshun9460@gmail.com>
2018-11-17 17:49:10 +0900

    update server list

--- other/com/hiyoko/dodontof/serverList.js
+++ other/com/hiyoko/dodontof/serverList.js
@@ -2,16 +2,15 @@ var com = com || {};
 com.hiyoko = com.hiyoko || {};
 com.hiyoko.DodontoF = com.hiyoko.DodontoF || {};
 com.hiyoko.DodontoF.PUBLIC_SERVER_LIST = {
-		'公式第三世代第壱鯖':'https://www.taruki.com/DodontoF_srv1/DodontoF.swf',
-		'公式第三世代第弐鯖':'https://www.taruki.com/DodontoF_srv2/DodontoF.swf',
-		'公式第三世代第参鯖':'https://www.taruki.com/DodontoF_srv3/DodontoF.swf',
-		'公式第三世代第四鯖':'https://www.taruki.com/DodontoF_srv4/DodontoF.swf',
-		'公式第三世代第五鯖':'https://www.taruki.com/DodontoF_srv5/DodontoF.swf',
-		'公式第三世代第六鯖':'https://www.taruki.com/DodontoF_srv6/DodontoF.swf',
+		'公式第三世代第壱鯖':'https://dodontof.onlinesession.app/DodontoF_srv1/DodontoF.swf',
+		'公式第三世代第弐鯖':'https://dodontof.onlinesession.app/DodontoF_srv2/DodontoF.swf',
+		'公式第三世代第参鯖':'https://dodontof.onlinesession.app/DodontoF_srv3/DodontoF.swf',
+		'公式第三世代第四鯖':'https://dodontof.onlinesession.app/DodontoF_srv4/DodontoF.swf',
+		'公式第三世代第五鯖':'https://dodontof.onlinesession.app/DodontoF_srv5/DodontoF.swf',
+		'公式第三世代第六鯖':'https://dodontof.onlinesession.app/DodontoF_srv6/DodontoF.swf',
 		'クラウドゲート公式':'http://onse01.wtrpg.com/DodontoF/DodontoF.swf',
 		'インコグ・ラボ公式':'http://dodontof.incoglab.com/DodontoF.swf',
 		'TRPG.NET 第1':'https://d1.trpg.net/DodontoF.swf',
 		'TRPG.NET 第2':'https://d2.trpg.net/DodontoF.swf',
-		'如月翔也さん':'http://trpg.gigafreaks.com/dodontof/DodontoF.swf',
-		'こかげサーバ':'https://dodontof.cokage.works/'
+		'如月翔也さん':'http://trpg.gigafreaks.com/dodontof/DodontoF.swf'
 };
