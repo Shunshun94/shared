d651289137539a57392a431d4e88ddf040cfd99e
Shunshun94 <shunshun9460@gmail.com>
2019-09-11 00:48:43 +0900

    change structure

--- vue/io/github/shunshun94/trpg/map/sharedMap.js
+++ vue/io/github/shunshun94/trpg/map/sharedMap.js
@@ -1,51 +1,23 @@
-const getQueries = () => {
-	const rawQuery = location.search.slice(1);
-	if(rawQuery === '') {
-		return {};
-	}
-	const query = decodeURI(rawQuery).split('&');
-	const paramLength = query.length;
-	let result = {};
-	for(var i = 0; i < paramLength; i++) {
-		const pair = query[i].split('=');
-		result[pair[0]] = pair.slice(1).join('=');
-	}
-	return result;
-};
-
-const query = getQueries();
-
-const data = {
-		characters:[],
-		range: {
-			x:-200,
-			y:0
-		}
-};
-const config = {
-	scale:25
-};
-
-let characterIndex = 0;
-for(var name in query) {
-	if(! Boolean(config[name])) {
-		let raw = query[name].split(',');
-		data.characters.push({
-			name: name,
-			x: Number(raw[0] || 0),
-			y: Number((raw.length >= 2) ?  raw[1] : 0),
-			color: ((raw.length >= 3) ?  raw[2] : 'C8C8C8'),
-			id: characterIndex
-		});
-		characterIndex++;
-	} else {
-		config[name] = query[name];
-	}
-}
-
-new Vue({
-	el: '#sharedMap',
-	data: data,
+Vue.component('shared-map', {
+	props:['data'],
+	template: `
+	<div>
+		<div id="sharedMap-menu">めにゅー</div>
+		<div id="sharedMap-map">
+			<shared-map-range
+				:x="data.range.x"
+				:y="data.range.y"
+			></shared-map-range>
+			<shared-map-character
+				v-for="character in data.characters"
+				:key="character.id"
+				:character="character"
+				v-on:shared-map-map-character-move-character="onMoveCharacter"
+				v-on:shared-map-map-character-dblclick-character="onDoubleClickCharacter"
+			></shared-map-character>
+		</div>
+	</div>	
+	`,
 	methods: {
 		onMoveCharacter: function(e) {
 			query[e.name.trim()] = `${e.x},${e.y},${e.color}`;
@@ -56,8 +28,9 @@ new Vue({
 			location.href = `${location.protocol}//${location.host}${location.pathname}?${params.join('&')}`;
 		},
 		onDoubleClickCharacter: function(e) {
-			this.range.x = e.x;
-			this.range.y = e.y;
+			this.data.range.x = e.x;
+			this.data.range.y = e.y;
 		}
 	}
 });
+
