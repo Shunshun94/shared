b9637768cdea8a8c67d5f93b6eef1cf28fbdbcad
Shunshun94 <shunshun9460@gmail.com>
2019-09-17 01:47:11 +0900

    add text scale

--- vue/io/github/shunshun94/trpg/map/sharedMapRange.js
+++ vue/io/github/shunshun94/trpg/map/sharedMapRange.js
@@ -1,36 +1,81 @@
 Vue.component('shared-map-range', {
 	props:['x', 'y', 'config'],
+	data: function() {
+		return {
+			posX: 0,
+			posY: 1
+		}
+	},
 	template: `
-	<div id="sharedMap-map-range"  :style="getPosition()">
+	<div
+		id="sharedMap-map-range"
+		:style="position"
+		@mousemove="onMouseMove">
 		<shared-map-range-circle
 			:range="Number(range)" :config="config"
 			v-for="range in config.circledRanges.split(',')"		
 		></shared-map-range-circle>
-		<hr class="sharedMap-map-range-line"/>
+
+		<hr style="transform: rotate(0deg);"
+			class="sharedMap-map-range-line"/>
+		<hr style="transform: rotate(90deg);"
+			class="sharedMap-map-range-line"/>
+		<hr style="transform: rotate(180deg);"
+			class="sharedMap-map-range-line"/>
+		<hr style="transform: rotate(270deg);"
+			class="sharedMap-map-range-line"/>
+
+		<hr :style="lineAngle"
+			class="sharedMap-map-range-line"/>
+
+		<shared-map-range-marker
+			:x="posX"
+			:y="posY"
+			:config="config"
+		></shared-map-range-marker>
 	</div>`,
-	methods: {
-		getPosition: function() {
+	computed: {
+		position: function() {
 			return `
 				left:${this.x * this.config.scale + 20}px;
 				top:${this.y * this.config.scale + 20}px;`
 		},
-		getStyle: function(width) {
-			return `
-				left:${this.x * this.config.scale}px;
-				top:${this.y * this.config.scale}px;
-				height:${width * this.config.scale}px;
-				width:${width * this.config.scale}px;
-				border-radius:${(width/2) * this.config.scale}px;
-				transform:translate(-${(width/2) * this.config.scale - 15}px, -${(width/2) * this.config.scale - 15}px);`;
+		lineAngle: function() {
+			let angle = Math.atan(- this.posX / this.posY);
+			if( this.posY >= 0 ) {
+				angle += Math.PI / 2;
+			} else {
+				angle -= Math.PI / 2;
+			}
+			return `transform: rotate(${angle}rad);`;
+		}
+	},
+	methods: {
+		onMouseMove: function(e) {
+			this.posX = e.layerX;
+			this.posY = e.layerY;
+		}
+	}
+});
+
+Vue.component('shared-map-range-marker', {
+	props:['x', 'y', 'config'],
+	template: `
+	<span
+		class="sharedMap-map-range-circle"
+		:style="position"
+	>{{ distance }}m</span>`,
+	computed: {
+		distance: function() {
+			const length = Math.sqrt( Math.pow(this.x, 2) + Math.pow(this.y, 2) ) / this.config.scale;
+			return (Math.round(length * 100)) / 100;
+			
 		},
-		getStyleInternal: function(width, outsideWidth) {
+		position: function() {
 			return `
-				left:${(outsideWidth / 2) * this.config.scale}px;
-				top:${(outsideWidth / 2) * this.config.scale}px;
-				height:${width * this.config.scale}px;
-				width:${width * this.config.scale}px;
-				border-radius:${(width/2) * this.config.scale}px;
-				transform:translate(-${(width/2) * this.config.scale}px, -${(width/2) * this.config.scale}px);`;
+				left: ${this.x}px;
+				top: ${this.y}px;
+			`;
 		}
 	}
 });
