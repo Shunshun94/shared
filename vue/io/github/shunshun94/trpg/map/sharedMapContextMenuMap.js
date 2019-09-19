50abb93dc7da86007abd9db26cd6b7ddb6c929f2
Shunshun94 <shunshun9460@gmail.com>
2019-09-20 00:12:36 +0900

    add range circle edit

--- vue/io/github/shunshun94/trpg/map/sharedMapContextMenuMap.js
+++ vue/io/github/shunshun94/trpg/map/sharedMapContextMenuMap.js
@@ -3,10 +3,12 @@ Vue.component('shared-map-context-menu-map', {
 	data: function() {
 		return {
 			consts: {
-				backgroundImage: '背景画像の URL を入力してください'
+				backgroundImage: '背景画像の URL を入力してください',
+				circledRanges: '距離の目安となる円の半径を入力してください（半角数字、カンマ区切り）'
 			},
 			defaultValue: {
-				backgroundImage: '-'
+				backgroundImage: '-',
+				circledRanges: '100,50,30,20,10,5,3'
 			}
 		}
 	},
@@ -15,14 +17,15 @@ Vue.component('shared-map-context-menu-map', {
 		class="shared-map-context-menu-map"
 		:style="generateContextMenuStyle()">
 			<div @click="addCharacter">キャラクターを追加する</div>
-			<div @click="updateBackgroundImage('backgroundImage')">背景画像を変更する</div>
+			<div @click="updateConfig('backgroundImage')">背景画像を変更する</div>
+			<div @click="updateConfig('circledRanges')">距離の目安を変更する</div>
 	</div>`,
 	methods: {
 		addCharacter: function(e) {
 			this.$emit(`shared-map-context-menu-map-add-character`);
 			e.stopPropagation();
 		},
-		updateBackgroundImage: function(colomn) {
+		updateConfig: function(colomn) {
 			let newValue = prompt(this.consts[colomn], this.config[colomn]);
 			if(newValue !== null) {
 				if(newValue === '') {
