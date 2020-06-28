df18afc0a20208c3705410203b6aebf7767e637a
Shunshun94 <shunshun9460@gmail.com>
2020-06-28 15:58:23 +0900

    implemented

--- other/io/github/shunshun94/trpg/ytsheet/ytsheetToText.js
+++ other/io/github/shunshun94/trpg/ytsheet/ytsheetToText.js
@@ -203,35 +203,42 @@ io.github.shunshun94.trpg.ytsheet.generateCharacterTextFromYtSheet2SwordWorldPC 
 	const languageColumnLength = 5;
 	result.push(`  ${'名称'.padStart(languageNameColumnLength - 2, ' ')}${'会話'.padStart(languageColumnLength - 2, ' ')}${'読文'.padStart(languageColumnLength - 2, ' ')}`);
 	for(var i = 0; i < languageLength; i++) {
-		const name = json[`language${i+1}`];
+		const name = json[`language${i+1}`] || '？？？';
 		const talk = json[`language${i+1}Talk`] ? '〇' : '　';
 		const read = json[`language${i+1}Read`] ? '〇' : '　';
 		result.push(`  ${name.padStart(languageNameColumnLength - name.length, ' ')}${talk.padStart(languageColumnLength - 1, ' ')}${read.padStart(languageColumnLength - 1, ' ')}`);
 	}
 
-	/*
 	result.push('●名誉点');
-	let totalHonor = Number(json.honor);
+	const historyLength = Number(json.historyNum || '0'); 
+	let totalHonor = 0;
+	let honorDiffCandidate = Number(json.honor);
 	const honorPrefix = '  ';
 	const honorLength = Number(json.honorItemsNum || '0');
 	const dishonorLength = Number(json.dishonorItemsNum || '0')
 	result.push(`${honorPrefix}未使用名誉点：${json.honor}`);
+	for(let i = 0; i < historyLength + 1; i++) {
+		totalHonor += Number(json[`history${i}Honor`] || '0');
+	}
 	for(let i = 0; i < honorLength; i++) {
 		if(json[`honorItem${i+1}`]) {
 			result.push(`${honorPrefix}${json[`honorItem${i+1}`]}：${json[`honorItem${i+1}Pt`] || 0}`);
-			totalHonor += Number(json[`honorItem${i+1}Pt`] || '0');
+			honorDiffCandidate += Number(json[`honorItem${i+1}Pt`] || '0');
 		}
 	}
 	for(let i = 0; i < dishonorLength; i++) {
 		if(json[`dishonorItem${i+1}`]) {
 			result.push(`${honorPrefix}${json[`dishonorItem${i+1}`]}：-${json[`dishonorItem${i+1}Pt`] || 0}`);
 			totalHonor -= Number(json[`dishonorItem${i+1}Pt`] || '0');
+			honorDiffCandidate -= Number(json[`dishonorItem${i+1}Pt`] || '0');
 		}
 	}
+	if(totalHonor !== honorDiffCandidate) {
+		result.push(`${honorPrefix}冒険者ランク(${json.rank})：${totalHonor - honorDiffCandidate}`);
+	}
 	result.push(`${honorPrefix}合計名誉点：${totalHonor}`);
-	result.push('');*/
+	result.push('');
 
-	const historyLength = Number(json.historyNum || '0');
 	if(historyLength) {
 		const historyCountColumnLength = 4;
 		const historyColumnLength = 20;
@@ -244,81 +251,4 @@ io.github.shunshun94.trpg.ytsheet.generateCharacterTextFromYtSheet2SwordWorldPC 
 
 
 	return result.join('\n');
-};
-
-io.github.shunshun94.trpg.ytsheet.consts = {};
-io.github.shunshun94.trpg.ytsheet.consts.maxLevel = 17;
-io.github.shunshun94.trpg.ytsheet.consts.levelSkills = {
-	craftEnhance : '練技',
-	craftAlchemy : '賦術',
-	craftCommand : '鼓咆',
-	craftSong : '呪歌',
-	craftRiding : '騎芸',
-	craftPotential : '魔装',
-	craftSeal : '呪印',
-	craftDignity : '貴格',
-	magicGramarye : '秘奥魔法',
-	craftDivination : '占瞳'
-};
-io.github.shunshun94.trpg.ytsheet.consts.skills = {
-	lvFig : 'ファイター',
-	lvGra : 'グラップラー',
-	lvFen : 'フェンサー',
-	lvSho : 'シューター',
-	lvSor : 'ソーサラー',
-	lvCon : 'コンジャラー',
-	lvPri : 'プリースト',
-	lvFai : 'フェアリーテイマー',
-	lvMag : 'マギテック',
-	lvSco : 'スカウト',
-	lvRan : 'レンジャー',
-	lvSag : 'セージ',
-	lvEnh : 'エンハンサー',
-	lvBar : 'バード',
-	lvRid : 'ライダー',
-	lvAlc : 'アルケミスト',
-	lvWar : 'ウォーリーダー',
-	lvMys : 'ミスティック',
-	lvDem : 'デーモンルーラー',
-	lvPhy : 'フィジカルマスター',
-	lvGri : 'グリモワール',
-	lvAri : 'アリストクラシー',
-	lvArt : 'アーティザン'
-};
-io.github.shunshun94.trpg.ytsheet.consts.card = {};
-io.github.shunshun94.trpg.ytsheet.consts.card.color = {
-	'Red': '赤',
-	'Gre': '緑',
-	'Bla': '黒',
-	'Whi': '白',
-	'Gol': '金'
-};
-io.github.shunshun94.trpg.ytsheet.consts.card.rank = ['B', 'A', 'S', 'SS'];
-io.github.shunshun94.trpg.ytsheet.consts.accessory = {};
-io.github.shunshun94.trpg.ytsheet.consts.accessory.part = {
-	Head:'頭',
-	Face:'顔',
-	Ear:'耳',
-	Neck:'首',
-	Back:'背中',
-	HandR:'右手',
-	HandL:'左手',
-	Waist:'腰',
-	Leg:'足',
-	Other:'他',
-	Other2:'他',
-	Other3:'他',
-	Other4:'他',
-};
-io.github.shunshun94.trpg.ytsheet.consts.magic = {
-	magicPowerSor:'真語魔法',
-	magicPowerCon:'操霊魔法',
-	magicPowerPri:'神聖魔法',
-	magicPowerMag:'魔動機術',
-	magicPowerFai:'妖精魔法',
-	magicPowerDem:'召異魔法',
-	magicPowerGri:'秘奥魔法',
-	magicPowerBar:'呪歌',
-	magicPowerAlc:'賦術',
-	magicPowerMys:'占瞳'
 };
