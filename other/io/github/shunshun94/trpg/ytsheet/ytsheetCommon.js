c162c35161f1b5372b63e98caa625e10833cc981
Shunshun94 <shunshun9460@gmail.com>
2020-12-26 17:04:56 +0900

    add system selector

--- other/io/github/shunshun94/trpg/ytsheet/ytsheetCommon.js
+++ other/io/github/shunshun94/trpg/ytsheet/ytsheetCommon.js
@@ -40,6 +40,22 @@ io.github.shunshun94.trpg.ytsheet.length = (str='') => {
 	return length;
 };
 
+io.github.shunshun94.trpg.ytsheet.SHEET_TYPE = {
+	DX3_PC: 'ytsheet_dx3_pc',
+	SW25_PC: 'ytsheet_sw25_pc',
+	SW25_ENEMY: 'ytsheet_sw25_enemy'
+};
+
+io.github.shunshun94.trpg.ytsheet.getSheetSystem = (sheet) => {
+	if(sheet.type) {
+		return io.github.shunshun94.trpg.ytsheet.SHEET_TYPE.SW25_ENEMY;
+	} else if(result.effect1Name) {
+		return io.github.shunshun94.trpg.ytsheet.SHEET_TYPE.DX3_PC;
+	} else {
+		return io.github.shunshun94.trpg.ytsheet.SHEET_TYPE.SW25_PC;
+	}
+};
+
 io.github.shunshun94.trpg.ytsheet._getColumnLength = (list, header) => {
 	return list.reduce((currentMax, targetEffect)=>{
 		const result = {};
