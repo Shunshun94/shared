63c04fd763651bfb67f428bdd93f2b412c651bd3
Shunshun94 <shunshun9460@gmail.com>
2022-01-08 21:40:51 +0900

    insert title by URL

--- jquery/io/github/shunshun94/trpg/logEditor/menu/addNewElementMenu.js
+++ jquery/io/github/shunshun94/trpg/logEditor/menu/addNewElementMenu.js
@@ -6,6 +6,11 @@ io.github.shunshun94.trpg.logEditor = io.github.shunshun94.trpg.logEditor || {};
 io.github.shunshun94.trpg.logEditor.menu = io.github.shunshun94.trpg.logEditor.menu || {};
 io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu = io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu || {};
 
+io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS = {
+    bgmKey: `${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-bgmCandList`,
+    bgKey : `${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-bggCandList`
+}
+
 io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.generateDom = (nameList = []) => {
     return `<div
         class="${io.github.shunshun94.trpg.logEditor.CLASSES.TMP_WINDOW} ${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}"
@@ -50,7 +55,9 @@ io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.generateDom = (nameLi
                     <tr>
                         <th style="text-align:right;">画像URL</th>
                         <td><input
+                            class="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-url"
                             id="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bg-url"
+                            list="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgList"
                             type="text" /></td>
                     </tr>
                     <tr>
@@ -63,6 +70,11 @@ io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.generateDom = (nameLi
                         class="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bg-exec"
                     >画像の切り替え作成</button></td></tr>
                 </table>
+                <datalist id="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgList">
+                ${JSON.parse(localStorage.getItem(io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS.bgKey) || '[]').map((n)=>{
+                    return '<option value="' + n.url + '">' + n.title + '</option>';
+                }).join('')}
+                </datalist>
             </div>
             <br/>
             <div class="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgm">
@@ -70,7 +82,9 @@ io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.generateDom = (nameLi
                     <tr>
                         <th style="text-align:right;">音楽URL</th>
                         <td><input
+                            class="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-url"
                             id="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgm-url"
+                            list="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgmList"
                             type="text" /></td>
                     </tr>
                     <tr>
@@ -89,8 +103,14 @@ io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.generateDom = (nameLi
                         class="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgm-exec"
                     >音楽の切り替え作成</button></td></tr>
                 </table>
+                <datalist id="${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgmList">
+                ${JSON.parse(localStorage.getItem(io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS.bgmKey) || '[]').map((n)=>{
+                    return '<option value="' + n.url + '">' + n.title + '</option>';
+                }).join('')}
+                </datalist>
             </div>
         </div></div>
+
     </div>`;
 };
 
@@ -107,10 +127,39 @@ io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.getPostObject = () =>
     };
 };
 
-io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.RegisterBgm = (title, url) => {
-
+io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.insertByUrl = (updatedDom) => {
+    const id = updatedDom.attr('id');
+    const regexpExecResult = /ytsheetOptions-(.+)-url/.exec(id);
+    if(! Boolean(regexpExecResult)) { return; }
+    const key = regexpExecResult[1];
+    const url = updatedDom.val();
+    const cand = JSON.parse(localStorage.getItem(io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS[`${key}Key`]) || '[]').filter((d)=>{
+        return d.url === url;
+    });
+    if(cand.length) {
+        $(`#${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-${key}-title`).val(cand[0].title);
+    }
 };
 
-io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.RegisterBg = (title, url) => {
+io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.registerItem = (title, url, key) => {
+    const list = JSON.parse(localStorage.getItem(key) || '[]');
+    list.unshift({
+        url: url,
+        title: title
+    });
+    const tmp = [];
+    const newList = list.filter((d)=>{
+        if(tmp.includes(d.url)) {return false;}
+        tmp.push(d.url);
+        return true;
+    });
+    localStorage.setItem(key, JSON.stringify(newList));
+};
 
+io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.registerBgm = (title, url) => {
+    io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.RegisterItem(title, url, io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS.bgmKey);
+};
+
+io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.registerBg = (title, url) => {
+    io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.RegisterItem(title, url, io.github.shunshun94.trpg.logEditor.menu.AddNewElementMenu.CONSTS.bgKey);
 };
