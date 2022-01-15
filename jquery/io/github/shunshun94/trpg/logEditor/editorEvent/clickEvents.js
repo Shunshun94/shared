9eac17c7e8e957db64ea902b2a811416cec530fd
Shunshun94 <shunshun9460@gmail.com>
2022-01-15 16:01:58 +0900

    move events to clickEvents.js

---
+++ jquery/io/github/shunshun94/trpg/logEditor/editorEvent/clickEvents.js
@@ -0,0 +1,140 @@
+var io = io || {};
+io.github = io.github || {};
+io.github.shunshun94 = io.github.shunshun94 || {};
+io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
+io.github.shunshun94.trpg.logEditor = io.github.shunshun94.trpg.logEditor || {};
+io.github.shunshun94.trpg.logEditor.Editor = io.github.shunshun94.trpg.logEditor.Editor || {};
+
+io.github.shunshun94.trpg.logEditor.PostClickedEvents = (self, clicked, targetPost) => {
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.DUPLICATE) ) {
+        self.duplicate(targetPost);
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.MERGE) ) {
+        self.copyToTrash([targetPost, targetPost.prev()]);
+        self.merge(targetPost);
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.DELETE) ) {
+        self.copyToTrash([targetPost]);
+        targetPost.remove();
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.RANDOM_ID) ) {
+        self.setRndId(targetPost);
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.TOGGLE_SUB) ) {
+        self.toggleSub(targetPost);
+    }
+};
+
+io.github.shunshun94.trpg.logEditor.GeneralClicedEvents = (self, clicked) => {
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.SAVE)) {
+        const targetBlock = clicked.parents(`.editBlock`);
+        self.openBackScreen();
+        self.openSaveScreen(targetBlock.attr('id'));
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.NAME_MENU) ) {
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        self.openBackScreen();
+        self.openNameConfigScreen();
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.NAME_MENU_WINDOW}-classInsert`) ) {
+        const tr = clicked.parents(`.${io.github.shunshun94.trpg.logEditor.CLASSES.NAME_MENU_WINDOW}-tr`);
+        self.insertClassToNameConfig(tr);
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.PREVIEW}-close`) ) {
+        self.closePreview();
+        return;
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.PREVIEW}-update`) ) {
+        self.showPreview();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.NAME_MENU_EXEC) ) {
+        self.applyNameConfig();
+        self.closeTmpWindow(clicked);
+        return;
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save`) ) {
+        self.closeTmpWindow(clicked);
+        const buttonValue = clicked.val().split(' ');
+        const targetBlockType = buttonValue[0];
+        const targetExporter = buttonValue[1];
+        io.github.shunshun94.trpg.logEditor.export.getExporter(targetExporter).exec(
+            $(`#${targetBlockType}`).find('.logList'),
+            self.head,
+            self.omit,
+            io.github.shunshun94.trpg.logEditor.DOMS.BODY.attr('class'));
+        return;
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-exec`)) {
+        self.insertNewElement();
+        self.closeTmpWindow(clicked);
+        return;
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bg-exec`)) {
+        self.insertUpdateBackGroundPicture();
+        self.closeTmpWindow(clicked);
+        return;
+    }
+    if( clicked.hasClass(`${io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU_WINDOW}-ytsheetOptions-bgm-exec`)) {
+        self.insertUpdateBackGroundMusic();
+        self.closeTmpWindow(clicked);
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.STYLE_RESET_MENU) ) {
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        self.styleReset();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.ID_INSERTION_MENU) ) {
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        self.insertIdToHs();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.DARKMODE_MENU) ) {
+        io.github.shunshun94.trpg.logEditor.DOMS.BODY.toggleClass(io.github.shunshun94.trpg.logEditor.CLASSES.DARKMODE);
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.POPUP_MENU_TOGGLE) ) {
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.SYSTEM_TO_POST_MENU) ) {
+        self.convertSystemToPost();
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.ADD_FILE_MENU) ) {
+        io.github.shunshun94.trpg.logEditor.FileLoader.kickOneTimeSave();
+        io.github.shunshun94.trpg.logEditor.menu.PopupMenu.toggle();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.SAME_MEMBER_MENU) ) {
+        self.searchSameMemberDoublePost();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.PREVIEW_MENU) ) {
+        self.showPreview();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.ADD_ELEMENT_MENU) ) {
+        self.openBackScreen();
+        self.showAddElementMenu();
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.TRASHBOX_MENU)) {
+        $('#trashbox').toggle(400);
+        $('#tmpEditorB').toggle(400);
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.BACKSCREEN) ) {
+        self.closeTmpWindow(clicked);
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.TRASHBOX_CLOSE)) {
+        $('#trashbox').hide(400);
+        $('#tmpEditorB').show(400);
+        return;
+    }
+    if( clicked.hasClass(io.github.shunshun94.trpg.logEditor.CLASSES.DELETE_ALL) ) {
+        $('#trashbox .logList .io-github-shunshun94-trpg-logEditor-Post').remove();
+    }
+};
