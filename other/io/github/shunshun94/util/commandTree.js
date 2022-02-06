bd4fd29d37a1cc798da31bf369aaf073d55bc791
Shunshun94 <shunshun9460@gmail.com>
2022-02-07 00:53:26 +0900

    basic implementation of commandTree is done

--- other/io/github/shunshun94/util/commandTree.js
+++ other/io/github/shunshun94/util/commandTree.js
@@ -4,33 +4,42 @@ io.github.shunshun94 = io.github.shunshun94 || {};
 io.github.shunshun94.util = io.github.shunshun94.util || {};
 io.github.shunshun94.util.CommandTree = io.github.shunshun94.util.CommandTree || {};
 
-io.github.shunshun94.util.CommandTree.CONSTS = {
-    OUTPUT_TYPES: {
-        JSON: io.github.shunshun94.util.CommandTree.outputAsJson
-    }
-};
-
-io.github.shunshun94.util.CommandTree.generate._parseLinesRecursive = (lines, currentResult={}) => {
+io.github.shunshun94.util.CommandTree._parseLinesRecursive = (lines) => {
     const linesMap = {};
     lines.forEach((d) => {
         const target = d[0];
-        if(target.startsWith('$')) {
-
-        } else {
-            if(linesMap[target]) {
-                linesMap[target] = [];
-            }
-            linesMap[target].push(d.slice(1))
+        if(! linesMap[target]) {
+            linesMap[target] = [];
+        }
+        const newLine = d.slice(1);
+        if(newLine.length) {
+            linesMap[target].push(newLine);
         }
     });
+    const result = {};
+    for(const key in linesMap) {
+        const targetLines = linesMap[key];
+        if(targetLines.length) {
+            result[key] = io.github.shunshun94.util.CommandTree._parseLinesRecursive(targetLines);
+        } else {
+            result[key] = {};
+        }
+    }
+    return result;
 };
 
 io.github.shunshun94.util.CommandTree.generate = (input, exportType=io.github.shunshun94.util.CommandTree.CONSTS.OUTPUT_TYPES.JSON) => {
-    const commands = inputs.split('\n').map((l)=>{return l.trim().split(' ')});
-    const parseResult = io.github.shunshun94.util.CommandTree.generate._parseLinesRecursive(commands);
+    const commands = input.split('\n').filter((l)=>{return l;}).map((l)=>{return l.trim().split(' ')});
+    const parseResult = io.github.shunshun94.util.CommandTree._parseLinesRecursive(commands);
     return exportType(parseResult);
 };
 
 io.github.shunshun94.util.CommandTree.outputAsJson = (input) => {
     return input;
+};
+
+io.github.shunshun94.util.CommandTree.CONSTS = {
+    OUTPUT_TYPES: {
+        JSON: io.github.shunshun94.util.CommandTree.outputAsJson
+    }
 };
