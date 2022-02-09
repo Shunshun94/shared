0efec5e3c754e40c7fb1ee4735b9f12b14bc8eac
Shunshun94 <shunshun9460@gmail.com>
2022-02-10 02:27:11 +0900

    Add javacord_slash

--- other/io/github/shunshun94/util/commandTree.js
+++ other/io/github/shunshun94/util/commandTree.js
@@ -84,12 +84,55 @@ io.github.shunshun94.util.CommandTree.outputAsHtml = (object) => {
     return io.github.shunshun94.util.CommandTree.generateHtmlRecursive(object, depth);
 };
 
+io.github.shunshun94.util.CommandTree.hasProperty = (object) => {
+    for(const key in object) {return true;}
+    return false;
+};
+
+io.github.shunshun94.util.CommandTree.generateJavacordSlashCommand = (object, key) => {
+    if(io.github.shunshun94.util.CommandTree.hasProperty(object)) {
+        const subCommandsList = [];
+        for(const subCommand in object) {
+            subCommandsList.push(io.github.shunshun94.util.CommandTree.generateJavacordSlashCommandRecursive(object[subCommand], subCommand, 1));
+        }
+        return `SlashCommand.with("${key}", "説明", Arrays.asList(\n${subCommandsList.join(',\n')}));`
+    } else {
+        return `SlashCommand.with("${key}", "説明");`;
+    }
+};
+
+io.github.shunshun94.util.CommandTree.generateJavacordSlashCommandRecursive = (object, tmp_key, depth) => {
+    if(io.github.shunshun94.util.CommandTree.hasProperty(object)) {
+        const optionType = tmp_key.startsWith('$') ? 'STRING' : 'SUB_COMMAND_GROUP';
+        const key = tmp_key.startsWith('$') ? tmp_key.substr(1) : tmp_key;
+        const subCommandsList = [];
+        for(const subCommand in object) {
+            subCommandsList.push(io.github.shunshun94.util.CommandTree.generateJavacordSlashCommandRecursive(object[subCommand], subCommand, depth + 1));
+        }
+        return `${'\t'.repeat(depth)}SlashCommandOption.createWithOptions(SlashCommandOptionType.${optionType}, "${key}", "説明", Arrays.asList(\n${subCommandsList.join(',\n')}))`;
+    } else {
+        const optionType = tmp_key.startsWith('$') ? 'STRING' : 'SUB_COMMAND';
+        const key = tmp_key.startsWith('$') ? tmp_key.substr(1) : tmp_key;
+        const suffix = tmp_key.startsWith('$') ? ', true' : '';
+        return `${'\t'.repeat(depth)}SlashCommandOption.create(SlashCommandOptionType.${optionType}, "${key}", "説明"${suffix})`
+    }
+};
+
+io.github.shunshun94.util.CommandTree.outputAsJavacordSlashCommand = (object) => {
+  const result = [];
+  for(const key in object) {
+      result.push(io.github.shunshun94.util.CommandTree.generateJavacordSlashCommand(object[key], key));
+  }
+  return result.join('\n\n');
+};
+
 io.github.shunshun94.util.CommandTree.CONSTS = {
     CLASS_NAME: {
         EMPTY : 'io-github-shunshun94-util-CommandTree-emptyColumn'
     },
     OUTPUT_TYPES: {
         JSON: io.github.shunshun94.util.CommandTree.outputAsJson,
-        HTML: io.github.shunshun94.util.CommandTree.outputAsHtml
+        HTML: io.github.shunshun94.util.CommandTree.outputAsHtml,
+        JAVACORD_SLASH: io.github.shunshun94.util.CommandTree.outputAsJavacordSlashCommand
     }
 };
