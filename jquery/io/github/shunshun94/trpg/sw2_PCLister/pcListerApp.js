72f60db52e7d6b9bff3bf7e5d72bc58d29fa6266
Shunshun94 <shunshun9460@gmail.com>
2022-03-04 01:46:36 +0900

    remove button is added

--- jquery/io/github/shunshun94/trpg/sw2_PCLister/pcListerApp.js
+++ jquery/io/github/shunshun94/trpg/sw2_PCLister/pcListerApp.js
@@ -4,12 +4,25 @@ io.github.shunshun94 = io.github.shunshun94 || {};
 io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
 io.github.shunshun94.trpg.SW2_PCListerApp = io.github.shunshun94.trpg.SW2_PCListerApp || {};
 
+io.github.shunshun94.trpg.SW2_PCListerApp.appendBuffTableDataTr = () => {
+    const tr = $('<tr></tr>');
+    tr.append(`<td><input type="text" class="buff-name" /></td>`);
+    ['hit', 'dodge', 'magic', 'mental', 'life', 'hp', 'guard', 'initiative', 'enemy'].forEach((d)=>{
+        tr.append(`<td><input type="number" value="0" class="buff-${d}" /></td>`);
+    });
+    tr.append('<td><button class="remmoveButton">✕</button></td>');
+    $('#buffTable-appendTr').before(tr);
+};
+
 io.github.shunshun94.trpg.SW2_PCListerApp.generateTr = (data) => {
     const tr = $('<tr></tr>');
+    const cId = com.hiyoko.util.rndString();
 
     const name = $('<td></td>');
     const nameA = $('<a></a>');
-    nameA.text('🔗' + data.name);
+    nameA.text(data.name);
+    name.addClass(cId);
+    name.addClass('name');
     nameA.attr('href', data.url);
     nameA.attr('target', '_blank');
     name.append(nameA);
@@ -21,7 +34,7 @@ io.github.shunshun94.trpg.SW2_PCListerApp.generateTr = (data) => {
        td.text(data[d] === 0 ? '0' : data[d] || '');
        tr.append(td);
     });
-    tr.find('.remove').append('<button class="remmoveButton">✕</button>')
+    tr.find('.remove').append('<button class="remmoveButton">✕</button>');
     return tr;
 };
 
@@ -33,6 +46,13 @@ io.github.shunshun94.trpg.SW2_PCListerApp.bindEvents = () => {
             $('.baseTable').append(io.github.shunshun94.trpg.SW2_PCListerApp.generateTr(data));
             com.hiyoko.util.updateLocalStorage('com-hiyoko-sample-sw2sheetparse-index', data.url, data.name);
         });
+    });
+    $('#buffTable-appendTr-exec').click(io.github.shunshun94.trpg.SW2_PCListerApp.appendBuffTableDataTr);
+    $('.output').click((e)=>{
+        const target = $(e.target);
+        if(target.hasClass('remmoveButton')) {
+            target.parent().parent().remove();
+        }
     });
 };
 
