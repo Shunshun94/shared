ee2dd12d7420f7f0e4587593a0d4358ff0ed1cf4
Shunshun94 <shunshun9460@gmail.com>
2022-04-12 02:44:18 +0900

    inport / export is implmented

--- vue/io/github/shunshun94/trpg/bufferTable/characters/characterSaveManager.js
+++ vue/io/github/shunshun94/trpg/bufferTable/characters/characterSaveManager.js
@@ -1,7 +1,7 @@
 Vue.component('character-save-manager', {
     template: `<div id="io-github-shunshun94-trpg-bufferTable-characters-savemanager">
         <button @click="saveAsTable">表の HTML をクリップボードにコピー</button><br/>
-        <button @click="saveAsJson">表を json として保存</button>
+        <button @click="saveAsJson">表を json として保存</button>:<a href="https://shunshun94.github.io/shared/jquery/io/github/shunshun94/trpg/sw2_PCLister/lister.html" target="_blank">ソードワールド2.X PC 能力比較・一覧</a>にドラッグ・ドロップでインポートが可能です
     </div>`,
     methods: {
         saveAsTable: function(e) {
