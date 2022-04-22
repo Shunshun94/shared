d0335b742540dae929707d87b77f94e9ecf79573
Shunshun94 <shunshun9460@gmail.com>
2022-04-23 02:40:44 +0900

    add common dice roll result exporter

--- jquery/io/github/shunshun94/trpg/logEditor/exporter/operationTable.js
+++ jquery/io/github/shunshun94/trpg/logEditor/exporter/operationTable.js
@@ -5,64 +5,8 @@ io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
 io.github.shunshun94.trpg.logEditor = io.github.shunshun94.trpg.logEditor || {};
 io.github.shunshun94.trpg.logEditor.export = io.github.shunshun94.trpg.logEditor.export || {};
 io.github.shunshun94.trpg.logEditor.export.OperationTableExporter = io.github.shunshun94.trpg.logEditor.export.OperationTableExporter || {};
-io.github.shunshun94.trpg.logEditor.export.OperationJsonExporter = io.github.shunshun94.trpg.logEditor.export.OperationJsonExporter || {};
-
 io.github.shunshun94.trpg.logEditor.export.OperationTableExporter.CONSTS = io.github.shunshun94.trpg.logEditor.export.OperationTableExporter.CONSTS || {};
-
-io.github.shunshun94.trpg.logEditor.export.OperationTableExporter.CONSTS.SESSION_ELEMENT_HANDLERS = [
-    {
-        name: 'commonResourceManage',
-        getMatchResult: (post)=>{
-            return (/\[\s(.+)\s\]\s(.+)\s:\s(\d+)\s→\s(\d+)/gm).exec(post.content);
-        },
-        getTableData: (post, matchResult)=> {
-            const diff = Number(matchResult[4]) - Number(matchResult[3]);
-            const name = matchResult[1];
-            return {
-                name: name,
-                paramUpdate: {
-                    column: matchResult[2],
-                    before: matchResult[3],
-                    after:  matchResult[4],
-                    diff:   diff
-                },
-                before: {
-                    name: post.name,
-                    content: post.content.substring(0, matchResult.index).trim()
-                },
-                after: {
-                    name: post.name,
-                    content: post.content.substring(matchResult.index).replace(matchResult[0], '').trim()
-                }
-            };
-        }
-    }, {
-        name: 'editedResourceManage',
-        getMatchResult: (post)=>{
-            return (/([^\t\n\r]+)\s:\s(\d+)\s→\s(\d+)/gm).exec(post.content);
-        },
-        getTableData: (post, matchResult)=> {
-            const diff = Number(matchResult[3]) - Number(matchResult[2]);
-            return {
-                name: post.name.trim(),
-                paramUpdate: {
-                    column: matchResult[1],
-                    before: matchResult[2],
-                    after:  matchResult[3],
-                    diff:   diff
-                },
-                before: {
-                    name: post.name,
-                    content: post.content.substring(0, matchResult.index).trim()
-                },
-                after: {
-                    name: post.name,
-                    content: post.content.substring(matchResult.index).replace(matchResult[0], '').trim()
-                }
-            };
-        }
-    }
-];
+io.github.shunshun94.trpg.logEditor.export.OperationJsonExporter = io.github.shunshun94.trpg.logEditor.export.OperationJsonExporter || {};
 
 io.github.shunshun94.trpg.logEditor.export.OperationTableExporter.getSessionElement = (post) => {
     const filteredElement = io.github.shunshun94.trpg.logEditor.export.OperationTableExporter.CONSTS.SESSION_ELEMENT_HANDLERS.map((element)=>{
