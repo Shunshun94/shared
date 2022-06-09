31703a2f6f414f293ec8781964209523255ba66a
Shunshun94 <shunshun9460@gmail.com>
2022-06-10 02:21:22 +0900

    fix for custom

--- other/io/github/shunshun94/trpg/avandner/map/map.js
+++ other/io/github/shunshun94/trpg/avandner/map/map.js
@@ -20,46 +20,8 @@ io.github.shunshun94.trpg.avandner.map.generate = (option={}) => {
     ].join('\n');
 };
 
-io.github.shunshun94.trpg.avandner.map.generateCharactersByPositions = (characters={}) => {
-    const result = {};
-    for(var key in characters) {
-        const pos = characters[key];
-        if(! result[pos]) {
-            result[pos] = [];
-        }
-        result[pos].push(key);
-    }
-    return result;
-};
-
-io.github.shunshun94.trpg.avandner.map.generateCharacterPositions = (option={}) => {
-    const characterByPos = option.charactersByPositions || io.github.shunshun94.trpg.avandner.map.generateCharactersByPositions(option.characters);
-    const mapBaseData = io.github.shunshun94.trpg.avandner.map.CONSTS.mapBaseData;
-    const characterFontSize = option.characterFontSize || option.fontSize || io.github.shunshun94.trpg.avandner.map.CONSTS.CHAR_FONT_SIZE;
-    const result = [];
-    for(var pos in characterByPos) {
-        const characters = characterByPos[pos].join(',');
-        const postionRegExpResults = [
-            { re:/^(\d+)[iI]/.exec(pos), length: (mapBaseData.center.length + mapBaseData.in.length ) / 2 },
-            { re:/^(\d+)[oO]/.exec(pos), length: (mapBaseData.in.length     + mapBaseData.out.length) / 2 },
-            { re:/^[cC]/.exec(pos),      length: 0}
-        ].filter((d)=>{return d.re});
-        if(postionRegExpResults.length === 0) {
-            throw `位置情報 "${pos}" は無効な位置情報です。位置情報は 8i や 6o、ないし c といった記法である必要があります`;
-        }
-        const postionRegExpResult = postionRegExpResults[0];
-        const baseLength = postionRegExpResult.length;
-        const position   = Number(postionRegExpResult.re[1] || '0') - 3;
-
-        const baseX = baseLength * Math.cos((Math.PI * (position*2))/12) - ((characterFontSize / 2) * characters.length);
-        const baseY = baseLength * Math.sin((Math.PI * (position*2))/12) - ( characterFontSize / 2);
-        result.push(`<text x="${baseX}" y="${baseY}" >${characters}</text>`);
-    }
-    return `<g font-size="${characterFontSize}">\n${result.join('\n')}\n</g>`;
-};
-
 io.github.shunshun94.trpg.avandner.map.CONSTS = io.github.shunshun94.trpg.avandner.map.CONSTS || {};
-io.github.shunshun94.trpg.avandner.map.CONSTS.CHAR_FONT_SIZE = 8;
+io.github.shunshun94.trpg.avandner.map.CONSTS.CHAR_FONT_SIZE =  8;
 io.github.shunshun94.trpg.avandner.map.CONSTS.NUMS_FONT_SIZE = 14;
 io.github.shunshun94.trpg.avandner.map.CONSTS.NUMS_TEXTS = ['１', '２', '３', '４', '５', '６', '７', '８', '９', '10', '11', '12'];
 io.github.shunshun94.trpg.avandner.map.CONSTS.mapBaseData = {
@@ -113,4 +75,42 @@ io.github.shunshun94.trpg.avandner.map.generateLabels = (option={}) => {
         const y = baseLength * Math.sin((Math.PI * (i*2))/12) + (numberLabelFontSize / 2);
         return `<text x="${x}" y="${y}" >${t}</text>`;
     }).join('\n') + '\n</g>';    
+};
+
+io.github.shunshun94.trpg.avandner.map.generateCharactersByPositions = (characters={}) => {
+    const result = {};
+    for(var key in characters) {
+        const pos = characters[key];
+        if(! result[pos]) {
+            result[pos] = [];
+        }
+        result[pos].push(key);
+    }
+    return result;
+};
+
+io.github.shunshun94.trpg.avandner.map.generateCharacterPositions = (option={}) => {
+    const characterByPos = option.charactersByPositions || io.github.shunshun94.trpg.avandner.map.generateCharactersByPositions(option.characters);
+    const mapBaseData = io.github.shunshun94.trpg.avandner.map.CONSTS.mapBaseData;
+    const characterFontSize = option.characterFontSize || option.fontSize || io.github.shunshun94.trpg.avandner.map.CONSTS.CHAR_FONT_SIZE;
+    const result = [];
+    for(var pos in characterByPos) {
+        const characters = characterByPos[pos].join(',');
+        const postionRegExpResults = [
+            { re:/^(\d+)[iI]/.exec(pos), length: (mapBaseData.center.length + mapBaseData.in.length ) / 2 },
+            { re:/^(\d+)[oO]/.exec(pos), length: (mapBaseData.in.length     + mapBaseData.out.length) / 2 },
+            { re:/^[cC]/.exec(pos),      length: 0}
+        ].filter((d)=>{return d.re});
+        if(postionRegExpResults.length === 0) {
+            throw `位置情報 "${pos}" は無効な位置情報です。位置情報は 8i や 6o、ないし c といった記法である必要があります`;
+        }
+        const postionRegExpResult = postionRegExpResults[0];
+        const baseLength = postionRegExpResult.length;
+        const position   = Number(postionRegExpResult.re[1] || '0') - 3;
+
+        const baseX = baseLength * Math.cos((Math.PI * (position*2))/12) - ((characterFontSize / 2) * characters.length);
+        const baseY = baseLength * Math.sin((Math.PI * (position*2))/12) - ( characterFontSize / 2);
+        result.push(`<text x="${baseX}" y="${baseY}" >${characters}</text>`);
+    }
+    return `<g font-size="${characterFontSize}">\n${result.join('\n')}\n</g>`;
 };
