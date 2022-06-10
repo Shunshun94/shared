0fc42aeee06da97e08105306e6b76f10adcb726f
Shunshun94 <shunshun9460@gmail.com>
2022-06-11 04:21:55 +0900

    add path info

--- other/io/github/shunshun94/trpg/avandner/map/map.js
+++ other/io/github/shunshun94/trpg/avandner/map/map.js
@@ -18,18 +18,6 @@ io.github.shunshun94.trpg.avandner.map.generate = (option={}) => {
     ].join('\n');
 };
 
-io.github.shunshun94.trpg.avandner.map.generateMove = (option={}) => {
-    if(! option.move) {return '';}
-    const result = [];
-    const color = option.moveColor || option.strokeColor || option.color || '#D0D0D0';
-    for(var i = 0; i < option.move.length - 1; i++) {
-        const startPosition = io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[i]);
-        const endPosition = io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[i + 1]);
-        result.push(`<line class="io-github-shunshun94-trpg-avandner-map-move" x1="${startPosition.x}" y1="${startPosition.y}" x2="${endPosition.x}" y2="${endPosition.y}" stroke="${color}" />`);
-    }
-    return result.join('\n');
-};
-
 io.github.shunshun94.trpg.avandner.map.CONSTS = io.github.shunshun94.trpg.avandner.map.CONSTS || {};
 io.github.shunshun94.trpg.avandner.map.CONSTS.CHAR_FONT_SIZE =  8;
 io.github.shunshun94.trpg.avandner.map.CONSTS.NUMS_FONT_SIZE = 14;
@@ -135,4 +123,67 @@ io.github.shunshun94.trpg.avandner.map.generateCharacterPositions = (option={}) 
         result.push(`<text class="io-github-shunshun94-trpg-avandner-map-characterLabel-${pos}" x="${baseX}" y="${baseY}" >${characters}</text>`);
     }
     return `<g class="io-github-shunshun94-trpg-avandner-map-characterLabel" font-size="${characterFontSize}">\n${result.join('\n')}\n</g>`;
+};
+
+io.github.shunshun94.trpg.avandner.map.calcTwoPointRelation = (start, end) => {
+    const pointLength = {
+        x: start.x - end.x,
+        y: start.y - end.y
+    };
+    const length = Math.sqrt(Math.pow(pointLength.x, 2) + Math.pow(pointLength.y, 2));
+    const angles  = {
+        sin: Math.asin((pointLength.y) / length),
+        cos: Math.acos((pointLength.x) / length)
+    };
+    const result = {
+        start:  start,
+        end:    end,
+        length: length
+    };
+    if(       pointLength.y >= 0 && pointLength.x <= 0 ) {
+        result.angle = angles.cos;
+    }else if( pointLength.y <= 0 && pointLength.x <= 0) {
+        result.angle = angles.sin - Math.PI / 2;
+    }else if( pointLength.x <= 0 && pointLength.y >= 0) {
+        result.angle = angles.cos - Math.PI / 2;
+    } else {
+        result.angle = angles.sin;
+    }
+    return result;
+};
+
+io.github.shunshun94.trpg.avandner.map.generateMove = (option={}) => {
+    if(! option.move) {return '';}
+    const result = [];
+    const color = option.moveColor || option.strokeColor || option.color || '#D0D0D0';
+    for(var i = 0; i < option.move.length - 1; i++) {
+        const startPosition = io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[i]);
+        const endPosition = io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[i + 1]);
+        result.push(`<line class="io-github-shunshun94-trpg-avandner-map-move" x1="${startPosition.x}" y1="${startPosition.y}" x2="${endPosition.x}" y2="${endPosition.y}" stroke="${color}" />`);
+    }
+
+    const lastMove = io.github.shunshun94.trpg.avandner.map.calcTwoPointRelation(
+        io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[option.move.length - 2]),
+        io.github.shunshun94.trpg.avandner.map.calcCenterPositionByArea(option.move[option.move.length - 1])
+    );
+
+    result.push([
+        `<line class="io-github-shunshun94-trpg-avandner-map-move-finishPoints"`,
+        `x1="${lastMove.end.x}"`,
+        `x2="${lastMove.end.x + (lastMove.length / 3) * Math.sin(lastMove.angle - Math.PI / 12)}"`,
+        `y1="${lastMove.end.y}"`,
+        `y2="${lastMove.end.y + (lastMove.length / 3) * Math.cos(lastMove.angle - Math.PI / 12)}"`,
+        `stroke="${color}" />`
+    ].join(' '));
+    result.push([
+        `<line class="io-github-shunshun94-trpg-avandner-map-move-finishPoints"`,
+        `x1="${lastMove.end.x}"`,
+        `x2="${lastMove.end.x + (lastMove.length / 3) * Math.sin(lastMove.angle + Math.PI / 12)}"`,
+        `y1="${lastMove.end.y}"`,
+        `y2="${lastMove.end.y + (lastMove.length / 3) * Math.cos(lastMove.angle + Math.PI / 12)}"`,
+        `stroke="${color}" />`
+    ].join(' '));
+
+
+    return result.join('\n');
 };
