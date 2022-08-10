ba3ae1dff11d3def903865b0f6a48259d84a236d
Shunshun94 <shunshun9460@gmail.com>
2022-08-11 01:31:22 +0900

    add skill table

---
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/pc2enemy/skillConsts.js
@@ -0,0 +1,703 @@
+var io = io || {};
+io.github = io.github || {};
+io.github.shunshun94 = io.github.shunshun94 || {};
+io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
+io.github.shunshun94.trpg.sw2 = io.github.shunshun94.trpg.sw2 || {};
+io.github.shunshun94.trpg.sw2.ytsheet = io.github.shunshun94.trpg.sw2.ytsheet || {};
+io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY = io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY || {};
+io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.CONSTS = io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.CONSTS || {};
+io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.CONSTS.BATTLE_SKILLS = io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.CONSTS.BATTLE_SKILLS || {};
+io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.CONSTS.BATTLE_SKILLS.LIST = {
+    "足さばき": {
+      timing: "常"
+    },
+    "追い打ち": {
+      timing: "常",
+      group: 'シールドバッシュ'
+    },
+    "ガーディアンⅠ": {
+      timing: "常",
+      group: 'かばう'
+    },
+    "ガーディアンⅡ": {
+      timing: "常",
+      group: 'かばう'
+    },
+    "かいくぐり": {
+      timing: "常",
+      skip: true
+    },
+    "回避行動Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "回避行動Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "頑強": {
+      timing: "常",
+      skip: true
+    },
+    "キャパシティ": {
+      timing: "常",
+      skip: true
+    },
+    "鼓咆陣率追加Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "鼓咆陣率追加Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "鼓咆陣率追加Ⅲ": {
+      timing: "常",
+      skip: true
+    },
+    "射手の体術": {
+      timing: "常",
+      skip: true
+    },
+    "終律増強": {
+      timing: "常",
+      group: '呪歌'
+    },
+    "呪歌追加Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "呪歌追加Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "呪歌追加Ⅲ": {
+      timing: "常",
+      skip: true
+    },
+    "スローイングⅠ": {
+      timing: "常",
+      group: '射撃'
+    },
+    "スローイングⅡ": {
+      timing: "常",
+      group: '射撃'
+    },
+    "双撃": {
+      timing: "常",
+      group: '2回攻撃'
+    },
+    "相克の標的": {
+      timing: "常"
+    },
+    "相克の別離": {
+      timing: "常"
+    },
+    "ターゲッティング": {
+      timing: "常",
+      group: '射撃'
+    },
+    "鷹の目": {
+      timing: "常",
+      group: '射撃'
+    },
+    "超頑強": {
+      timing: "常",
+      skip: true
+    },
+    "抵抗強化Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "抵抗強化Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "特殊楽器習熟": {
+      timing: "常",
+      skip: true
+    },
+    "跳び蹴り": {
+      timing: "常"
+    },
+    "投げ強化Ⅰ": {
+      timing: "常",
+      group: '投げ'
+    },
+    "投げ強化Ⅱ": {
+      timing: "常",
+      group: '投げ'
+    },
+    "二刀流": {
+      timing: "常",
+      skip: true
+    },
+    "二刀無双": {
+      timing: "常",
+      skip: true
+    },
+    "ハーモニー": {
+      timing: "常",
+      group: '呪歌'
+    },
+    "武器習熟Ａ／ソード": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／アックス": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／スピア": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／メイス": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／スタッフ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／フレイル": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／ウォーハンマー": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／絡み": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／格闘": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／投擲": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／ボウ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／クロスボウ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／ブロウガン": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ａ／ガン": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／ソード": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／アックス": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／スピア": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／メイス": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／スタッフ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／フレイル": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／ウォーハンマー": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／絡み": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／格闘": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／投擲": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／ボウ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／クロスボウ": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／ブロウガン": {
+      timing: "常",
+      skip: true
+    },
+    "武器習熟Ｓ／ガン": {
+      timing: "常",
+      skip: true
+    },
+    "武器の達人": {
+      timing: "常",
+      skip: true
+    },
+    "賦術強化Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "賦術強化Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "賦術全遠隔化": {
+      timing: "常",
+      group: '賦術'
+    },
+    "踏みつけ": {
+      timing: "常",
+      group: '投げ'
+    },
+    "ブロッキング": {
+      timing: "常",
+      group: 'かばう'
+    },
+    "変幻自在Ⅰ": {
+      timing: "常",
+      group: '複数宣言',
+      replace: '複数宣言＝2回'
+    },
+    "変幻自在Ⅱ": {
+      timing: "常",
+      replace: '複数宣言＝3回'
+    },
+    "防具習熟Ａ／金属鎧": {
+      timing: "常",
+      skip: true
+    },
+    "防具習熟Ａ／非金属鎧": {
+      timing: "常",
+      skip: true
+    },
+    "防具習熟Ａ／盾": {
+      timing: "常",
+      skip: true
+    },
+    "防具習熟Ｓ／金属鎧": {
+      timing: "常",
+      skip: true
+    },
+    "防具習熟Ｓ／非金属鎧": {
+      timing: "常",
+      skip: true
+    },
+    "防具習熟Ｓ／盾": {
+      timing: "常",
+      skip: true
+    },
+    "防具の達人": {
+      timing: "常",
+      skip: true
+    },
+    "魔晶石の達人": {
+      timing: "常",
+      skip: true
+    },
+    "魔法拡大の達人": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "マリオネット": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "魔力強化Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "魔力強化Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "命中強化Ⅰ": {
+      timing: "常",
+      skip: true
+    },
+    "命中強化Ⅱ": {
+      timing: "常",
+      skip: true
+    },
+    "両手利き": {
+      timing: "主",
+      group: '2回攻撃',
+      replace: '2回攻撃'
+    },
+    "連続賦術": {
+      timing: "常"
+    },
+    "練体の極意": {
+      timing: "常"
+    },
+    "ＭＰ軽減／ソーサラー": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／コンジャラー": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／プリースト": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／マギテック": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／フェアリーテイマー": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／ドルイド": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／デーモンルーラー": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／ウィザード": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "ＭＰ軽減／グリモワール": {
+      timing: "常",
+      group: '魔法適性'
+    },
+    "自己占瞳": {
+      timing: "常"
+    },
+    "占瞳操作": {
+      timing: "常"
+    },
+    "代償軽減Ⅰ": {
+      timing: "常"
+    },
+    "代償軽減Ⅱ": {
+      timing: "常"
+    },
+    "代償半減": {
+      timing: "常"
+    },
+    "魔導書習熟Ａ": {
+      timing: "常",
+      skip: true
+    },
+    "魔導書習熟Ｓ": {
+      timing: "常",
+      skip: true
+    },
+    "魔導書の達人": {
+      timing: "常",
+      skip: true
+    },
+    "魔器習熟Ａ": {
+      timing: "常",
+      skip: true
+    },
+    "魔器習熟Ｓ": {
+      timing: "常",
+      skip: true
+    },
+    "魔器の達人": {
+      timing: "常",
+      skip: true
+    },
+    "切り払い": {
+      timing: "常"
+    },
+    "無尽の盾": {
+      timing: "常"
+    },
+    "ファントムカウンター": {
+      timing: "常"
+    },
+    "急所狙い": {
+      timing: "常"
+    },
+    "心眼": {
+      timing: "常"
+    },
+    "零距離射撃": {
+      timing: "常",
+      group: '射撃'
+    },
+    "インファイトⅠ": {
+      timing: "宣"
+    },
+    "インファイトⅡ": {
+      timing: "宣"
+    },
+    "囮攻撃Ⅰ": {
+      timing: "宣"
+    },
+    "囮攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "カード軽減": {
+      timing: "宣",
+      group: '賦術'
+    },
+    "楽素転換": {
+      timing: "宣",
+      group: '呪歌'
+    },
+    "影矢": {
+      timing: "宣",
+      group: '射撃'
+    },
+    "カニングキャストⅠ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "カニングキャストⅡ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "かばうⅠ": {
+      timing: ["宣", "準"],
+      group: 'かばう'
+    },
+    "かばうⅡ": {
+      timing: ["宣", "準"],
+      group: 'かばう'
+    },
+    "斬り返しⅠ": {
+      timing: "宣"
+    },
+    "斬り返しⅡ": {
+      timing: "宣"
+    },
+    "牙折り": {
+      timing: "宣"
+    },
+    "クイックキャスト": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "クリティカルキャストⅠ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "クリティカルキャストⅡ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "牽制攻撃Ⅰ": {
+      timing: "宣",
+      replace: '手早い斬撃&lt;br&gt;命中力判定に+1のボーナス修正を得ます。この能力は、カテゴリ＜金属鎧＞を装備しているキャラクターには効果を持ちません。'
+    },
+    "牽制攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "牽制攻撃Ⅲ": {
+      timing: "宣"
+    },
+    "高度な柔軟性": {
+      timing: "宣"
+    },
+    "シールドバッシュⅠ": {
+      timing: "宣",
+      group: 'シールドバッシュ'
+    },
+    "シールドバッシュⅡ": {
+      timing: "宣",
+      group: 'シールドバッシュ'
+    },
+    "シャドウステップⅠ": {
+      timing: "宣"
+    },
+    "シャドウステップⅡ": {
+      timing: "宣"
+    },
+    "シュアパフォーマー": {
+      timing: "宣",
+      group: '呪歌'
+    },
+    "スキルフルプレイ": {
+      timing: "宣",
+      group: '呪歌'
+    },
+    "捨て身攻撃Ⅰ": {
+      timing: "宣"
+    },
+    "捨て身攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "捨て身攻撃Ⅲ": {
+      timing: "宣"
+    },
+    "先陣の才覚": {
+      timing: "準"
+    },
+    "全力攻撃Ⅰ": {
+      timing: "宣"
+    },
+    "全力攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "全力攻撃Ⅲ": {
+      timing: "宣"
+    },
+    "ダブルキャスト": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "挑発攻撃Ⅰ": {
+      timing: "宣"
+    },
+    "挑発攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "露払い": {
+      timing: "宣"
+    },
+    "ディフェンススタンス": {
+      timing: "宣"
+    },
+    "テイルスイングⅠ": {
+      timing: "宣"
+    },
+    "テイルスイングⅡ": {
+      timing: "宣"
+    },
+    "薙ぎ払いⅠ": {
+      timing: "宣"
+    },
+    "薙ぎ払いⅡ": {
+      timing: "宣"
+    },
+    "バイオレントキャストⅠ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "バイオレントキャストⅡ": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "必殺攻撃Ⅰ": {
+      timing: "宣"
+    },
+    "必殺攻撃Ⅱ": {
+      timing: "宣"
+    },
+    "必殺攻撃Ⅲ": {
+      timing: "宣"
+    },
+    "魔法拡大／威力確実化": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大／確実化": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大／数": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大／距離": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大／時間": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大／範囲": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法拡大すべて": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法収束": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔法制御": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "魔力撃": {
+      timing: "宣",
+      replaceFunction: (json)=>{
+        const magic = io.github.shunshun94.trpg.sw2.ytsheet.PC2ENEMY.getMagicInfo(json);
+        return `魔力撃＝＋${magic.max}ダメージ`;
+      }
+    },
+    "マルチアクション": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "鎧貫きⅠ": {
+      timing: "宣"
+    },
+    "鎧貫きⅡ": {
+      timing: "宣"
+    },
+    "鎧貫きⅢ": {
+      timing: "宣"
+    },
+    "乱撃Ⅰ": {
+      timing: "宣"
+    },
+    "乱撃Ⅱ": {
+      timing: "宣"
+    },
+    "双占瞳": {
+      timing: "宣"
+    },
+    "痛撃": {
+      timing: "宣"
+    },
+    "跳躍攻撃": {
+      timing: "宣"
+    },
+    "封印撃": {
+      timing: "宣"
+    },
+    "ヒットアンドアウェイ": {
+      timing: "宣"
+    },
+    "曲射": {
+      timing: "宣",
+      group: '射撃'
+    },
+    "デュアルアクション": {
+      timing: "宣",
+      group: '魔法適性'
+    },
+    "狙撃": {
+      timing: "主",
+      group: '射撃'
+    },
+    "ワードブレイク": {
+      timing: "主"
+    }
+};
