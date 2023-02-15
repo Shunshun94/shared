bbb86ff1b7dd058ec6a87be17335497ab4381168
Shunshun94 <shunshun9460@gmail.com>
2023-02-16 01:37:50 +0900

    first release hitsuji slack shedule sharing

--- other/io/github/shunshun94/util/timetree/common/common.js
+++ other/io/github/shunshun94/util/timetree/common/common.js
@@ -32,5 +32,81 @@ function getSchedules(days = 7) {
   }).flat().sort((a,b)=>{
     return a.sortKey - b.sortKey;
   });
-  console.log(schedules);
+  return schedules;
+}
+
+function modifySchedulesEasyToHandle(schedules) {
+  const result = [];
+  let cursorDay = '';
+  schedules.map((s)=>{
+    const attr = s.attributes;
+    const start = new Date(attr.start_at);
+    const end = new Date(attr.end_at);
+    return {
+      isAllDay: attr.all_day,
+      title: attr.title,
+      date: start.toLocaleDateString('ja-JP'),
+      start: start.toLocaleTimeString('ja-JP').slice(0,5),
+      end: end.toLocaleTimeString('ja-JP').slice(0,5)
+    }
+  }).forEach((s)=>{
+    if(s.date !== cursorDay) {
+      cursorDay = s.date;
+      result.push({
+        date: s.date,
+        allDay: [],
+        schedules: []
+      });
+    }
+    if( s.isAllDay ) {
+      result.at(-1).allDay.push(s);
+    } else {
+      result.at(-1).schedules.push(s);
+    }
+  });
+  return result;
+}
+
+function generateSemiMarkdownFormattedSchedule(schedules) {
+  return modifySchedulesEasyToHandle(schedules).map((daySchedule)=>{
+    const result = [];
+    result.push(`【${daySchedule.date}】`);
+    daySchedule.allDay.forEach((schedule)=>{
+      result.push(`　終日：${schedule.title}`);
+    });
+    daySchedule.schedules.forEach((schedule)=>{
+      result.push(`　${schedule.title}`);
+      result.push(`　　${schedule.start} ～ ${schedule.end}`);
+    });
+    return result.join('\n');
+  }).join('\n\n');
+}
+
+function sendScheduleToSlack(schedules) {
+  const webhook = PropertiesService.getScriptProperties().getProperty('SLACK_INCOMING_WEBHOOK');
+  const icon = PropertiesService.getScriptProperties().getProperty('SLACK_SCHEDULE_ICON') || ':カニ:';
+  const payload = {
+    'username': '今週のスケジュール',
+    'text': generateSemiMarkdownFormattedSchedule(schedules),
+    'icon_emoji': icon
+  };
+  const payloadString = JSON.stringify(payload);
+  console.log(`webhook URL: ${webhook.substring(0, 18)}*********${webhook.slice(-4)}`);
+  console.log(`payload:      ${payloadString}`);
+  let les = UrlFetchApp.fetch(webhook, {
+    'method': 'post',
+    'contentType': 'application/json',
+    'payload': payloadString,
+    "muteHttpExceptions" : true,
+  });
+
+  if( les.getResponseCode() !== 200 ) {
+    console.error(les.getResponseCode());
+    console.error(les.getContentText());
+  }
+}
+
+function execute() {
+  const schedules = getSchedules();
+  sendScheduleToSlack(schedules);
 }
