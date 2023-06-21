f589c95ad8e3b8d6acc050e019567d9239720092
Shunshun94 <shunshun9460@gmail.com>
2023-06-22 00:33:06 +0900

    make function simpler

--- other/io/github/shunshun94/util/DatetimePicker/datetimePicker.js
+++ other/io/github/shunshun94/util/DatetimePicker/datetimePicker.js
@@ -7,6 +7,21 @@ io.github.shunshun94.util.DateTimePicker = io.github.shunshun94.util.DateTimePic
 io.github.shunshun94.util.DateTimePicker.DATE_REGEXP = /([\^\-―～]?)([01]?[0-9])\s*[\/／月\-・]\s*([0-3]?[0-9])\s*日?([\^\-―～]?)[日月火水木金土\s\　]*/;
 io.github.shunshun94.util.DateTimePicker.TIME_REGEXP = /([012]?[0-9])\s*[\:：時]\s*([0-5][0-9])?\s*[分～~]*/
 
+io.github.shunshun94.util.DateTimePicker.pickDate = (dateRegexpResult) => {
+    const untilSign = dateRegexpResult[1] ? '～' : '';
+    return `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)}`
+};
+
+io.github.shunshun94.util.DateTimePicker.pickTime = (timeRegexpResult) => {
+    if(timeRegexpResult) {
+        const hours   = timeRegexpResult[1];
+        const minutes = timeRegexpResult[2];
+        return ` ${hours.padStart(2, 0)}:${minutes ? (minutes.padStart(2, 0)) : '00'}`;
+    } else {
+        return '';
+    }
+};
+
 io.github.shunshun94.util.DateTimePicker.pick = (input) => {
     const text = input.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
@@ -17,30 +32,13 @@ io.github.shunshun94.util.DateTimePicker.pick = (input) => {
     const timeRegexpResult = io.github.shunshun94.util.DateTimePicker.TIME_REGEXP.exec(textRemovedDate);
     const textRemovedTime = (timeRegexpResult ? textRemovedDate.replace(timeRegexpResult[0], '') : textRemovedDate).trim();
 
-    const untilSign = (dateRegexpResult || [0,0])[1] ? '～' : '';
-
-    if(timeRegexpResult && dateRegexpResult) {
-        if(timeRegexpResult[2]) {
-            return {
-                dateRegExp: dateRegexpResult,
-                timeRegExp: timeRegexpResult,
-                text: `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)} ${timeRegexpResult[1].padStart(2, 0)}:${timeRegexpResult[2].padStart(2, 0)}`,
-                datetimeRevmoed: textRemovedTime
-            };
-        } else {
-            return {
-                dateRegExp: dateRegexpResult,
-                timeRegExp: timeRegexpResult,
-                text: `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)} ${timeRegexpResult[1].padStart(2, 0)}:00`,
-                datetimeRevmoed: textRemovedTime
-            };
-        }
-    }
     if(dateRegexpResult) {
+        const date = io.github.shunshun94.util.DateTimePicker.pickDate(dateRegexpResult);
+        const time = io.github.shunshun94.util.DateTimePicker.pickTime(timeRegexpResult);
         return {
             dateRegExp: dateRegexpResult,
             timeRegExp: timeRegexpResult,
-            text: `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)}`,
+            text: `${date}${time}`,
             datetimeRevmoed: textRemovedTime
         };
     }
