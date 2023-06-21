97e20b9f6dc53b6a046b0be7e5ff5366bd496620
Shunshun94 <shunshun9460@gmail.com>
2023-06-22 01:05:00 +0900

    make smaler

--- other/io/github/shunshun94/util/DatetimePicker/datetimePicker.js
+++ other/io/github/shunshun94/util/DatetimePicker/datetimePicker.js
@@ -8,8 +8,12 @@ io.github.shunshun94.util.DateTimePicker.DATE_REGEXP = /([\^\-―～]?)([01]?[0-
 io.github.shunshun94.util.DateTimePicker.TIME_REGEXP = /([012]?[0-9])\s*[\:：時]\s*([0-5][0-9])?\s*[分～~]*/
 
 io.github.shunshun94.util.DateTimePicker.pickDate = (dateRegexpResult) => {
-    const untilSign = dateRegexpResult[1] ? '～' : '';
-    return `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)}`
+    if(dateRegexpResult) {
+        const untilSign = dateRegexpResult[1] ? '～' : '';
+        return `${untilSign}${dateRegexpResult[2].padStart(2, 0)}/${dateRegexpResult[3].padStart(2, 0)}`
+    } else {
+        return '';
+    }
 };
 
 io.github.shunshun94.util.DateTimePicker.pickTime = (timeRegexpResult) => {
@@ -26,26 +30,47 @@ io.github.shunshun94.util.DateTimePicker.pick = (input) => {
     const text = input.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
         return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
     }).trim();
-    const dateRegexpResult = io.github.shunshun94.util.DateTimePicker.DATE_REGEXP.exec(text);
-    const textRemovedDate = (dateRegexpResult ? text.replace(dateRegexpResult[0], '') : text).trim();
 
-    const timeRegexpResult = io.github.shunshun94.util.DateTimePicker.TIME_REGEXP.exec(textRemovedDate);
-    const textRemovedTime = (timeRegexpResult ? textRemovedDate.replace(timeRegexpResult[0], '') : textRemovedDate).trim();
+    const regexpAppliedText = [
+        io.github.shunshun94.util.DateTimePicker.applyDateRegExp,
+        io.github.shunshun94.util.DateTimePicker.applyTimeRegExp
+    ].reduce((currentValue, func)=>{
+        return func(currentValue);
+    }, {text: text});
 
-    if(dateRegexpResult) {
-        const date = io.github.shunshun94.util.DateTimePicker.pickDate(dateRegexpResult);
-        const time = io.github.shunshun94.util.DateTimePicker.pickTime(timeRegexpResult);
+    const date = regexpAppliedText.date;
+    const time = regexpAppliedText.time;
+
+    if(date) {
         return {
-            dateRegExp: dateRegexpResult,
-            timeRegExp: timeRegexpResult,
+            dateRegExp: regexpAppliedText.dateRegexpResult,
+            timeRegExp: regexpAppliedText.timeRegexpResult,
             text: `${date}${time}`,
-            datetimeRevmoed: textRemovedTime
+            datetimeRevmoed: regexpAppliedText.text
         };
     }
     return {
-        dateRegExp: dateRegexpResult,
-        timeRegExp: timeRegexpResult,
+        dateRegExp: regexpAppliedText.dateRegexpResult,
+        timeRegExp: regexpAppliedText.timeRegexpResult,
         text: '',
-        datetimeRevmoed: textRemovedTime
+        datetimeRevmoed: regexpAppliedText.text
     };
+};
+
+io.github.shunshun94.util.DateTimePicker.applyTimeRegExp = (currentMap) => {
+    const timeRegexpResult = io.github.shunshun94.util.DateTimePicker.TIME_REGEXP.exec(currentMap.text);
+    const textRemovedTime = (timeRegexpResult ? currentMap.text.replace(timeRegexpResult[0], '') : currentMap.text).trim();
+    currentMap.text = textRemovedTime;
+    currentMap.timeRegexpResult = timeRegexpResult;
+    currentMap.time = io.github.shunshun94.util.DateTimePicker.pickTime(timeRegexpResult);
+    return currentMap;
+};
+
+io.github.shunshun94.util.DateTimePicker.applyDateRegExp = (currentMap) => {
+    const dateRegexpResult = io.github.shunshun94.util.DateTimePicker.DATE_REGEXP.exec(currentMap.text);
+    const textRemovedDate = (dateRegexpResult ? currentMap.text.replace(dateRegexpResult[0], '') : currentMap.text).trim();
+    currentMap.text = textRemovedDate;
+    currentMap.dateRegexpResult = dateRegexpResult;
+    currentMap.date = io.github.shunshun94.util.DateTimePicker.pickDate(dateRegexpResult);
+    return currentMap;
 };
