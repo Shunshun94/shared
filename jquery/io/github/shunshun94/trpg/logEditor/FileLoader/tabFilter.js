5c846fa45464ddf915628995e87376aa7226be3a
Shunshun94 <shunshun9460@gmail.com>
2023-06-29 02:21:46 +0900

    filter is impled

--- jquery/io/github/shunshun94/trpg/logEditor/FileLoader/tabFilter.js
+++ jquery/io/github/shunshun94/trpg/logEditor/FileLoader/tabFilter.js
@@ -5,22 +5,58 @@ io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
 io.github.shunshun94.trpg.logEditor = io.github.shunshun94.trpg.logEditor || {};
 io.github.shunshun94.trpg.logEditor.FileLoader = io.github.shunshun94.trpg.logEditor.FileLoader || {};
 
+io.github.shunshun94.trpg.logEditor.FileLoader.CONST = io.github.shunshun94.trpg.logEditor.FileLoader.CONST || {};
+io.github.shunshun94.trpg.logEditor.FileLoader.CONST.ID = io.github.shunshun94.trpg.logEditor.CLASSES.FILTER + 'tabmanage';
+
 io.github.shunshun94.trpg.logEditor.FileLoader.filtTabs = (data) => {
     return new Promise((resolve, reject)=>{
         const tabs = data.tabs;
         const tabsCount = Object.keys(tabs).length;
-        const doms = data.doms;
         if(tabsCount > 2) {
             io.github.shunshun94.trpg.logEditor.FileLoader.openBackScreen();
             io.github.shunshun94.trpg.logEditor.FileLoader.openTabManageWindow(tabs);
             $(`.${io.github.shunshun94.trpg.logEditor.CLASSES.FILTER} > button`).click((e)=>{
+                const collectedTabs = io.github.shunshun94.trpg.logEditor.FileLoader.collectTabManageInputInfo();
+                data.doms = io.github.shunshun94.trpg.logEditor.FileLoader.filterDoms(data.doms, collectedTabs);
+                data.tabList = io.github.shunshun94.trpg.logEditor.FileLoader.collectUsedTabsClasses(collectedTabs);
                 resolve(data);
                 io.github.shunshun94.trpg.logEditor.FileLoader.closeTabManageWindow();
             });
         } else {
+            data.tabList = [];
             resolve(data);
         }
     });
+};
+
+io.github.shunshun94.trpg.logEditor.FileLoader.collectUsedTabsClasses = (collectedTabInfo) => {
+    const result = [];
+    for(var key in collectedTabInfo) {
+        result.push(collectedTabInfo[key].classes.split(' '));
+    }
+    return [...new Set(result.flat().filter((d)=>{return d}))];
+};
+
+io.github.shunshun94.trpg.logEditor.FileLoader.filterDoms = (doms, collectedTabInfo) => {
+    return doms.filter((dom)=>{
+        return collectedTabInfo[dom.tabName].isActive;
+    }).map((dom)=>{
+        dom.class = collectedTabInfo[dom.tabName].classes;
+        return dom;
+    });
+};
+
+io.github.shunshun94.trpg.logEditor.FileLoader.collectTabManageInputInfo = () => {
+    const result = {};
+    Array.from(document.getElementById(io.github.shunshun94.trpg.logEditor.FileLoader.CONST.ID).getElementsByTagName('tr')).forEach((tr)=>{
+        const tabName = tr.getElementsByTagName('label')[0].textContent;
+        const inputs = tr.getElementsByTagName('input');
+        result[tabName] = {
+            isActive: inputs[0].checked,
+            classes:  inputs[1].value
+        };
+    });
+    return result;
 };
 
 io.github.shunshun94.trpg.logEditor.FileLoader.closeTabManageWindow = () => {
@@ -49,10 +85,13 @@ io.github.shunshun94.trpg.logEditor.FileLoader.generateInputForTab = (tabName, i
     tabClass.setAttribute('type', 'text');
     if(index) {
         tabClass.setAttribute('value', `tab${index}`);
-        console.log(index, tabClass);
     }
 
-    [tabNameLabel, tabActivateCheckboxText, tabActivateCheckbox, tabClassText, tabClass].forEach((d)=>{
+    const beTab1 = document.createElement('button');
+    beTab1.className = 'tab1';
+    beTab1.textContent = 'tab1 にする';
+
+    [tabNameLabel, tabActivateCheckboxText, tabActivateCheckbox, tabClassText, tabClass, beTab1].forEach((d)=>{
         const td = document.createElement('td');
         td.append(d);
         tabInfo.append(td);
@@ -64,7 +103,7 @@ io.github.shunshun94.trpg.logEditor.FileLoader.generateInputForTab = (tabName, i
 io.github.shunshun94.trpg.logEditor.FileLoader.openTabManageWindow = (tabs) => {
     const result = document.createElement('div');
     result.className = io.github.shunshun94.trpg.logEditor.CLASSES.FILTER;
-    result.id = io.github.shunshun94.trpg.logEditor.CLASSES.FILTER + 'tabmanage';
+    result.id = io.github.shunshun94.trpg.logEditor.FileLoader.CONST.ID;
 
     const explainText = document.createElement('p');
     explainText.append('ログの各タブについて出力するか否かを設定してください。');
