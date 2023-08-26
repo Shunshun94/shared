eaa6e56e0ad1b13768416e7635eba5f29e37d1fc
Shunshun94 <shunshun9460@gmail.com>
2023-08-27 02:08:02 +0900

    add class named disableInConvert

--- jquery/io/github/shunshun94/trpg/logEditor/menu/saveMenu.js
+++ jquery/io/github/shunshun94/trpg/logEditor/menu/saveMenu.js
@@ -12,11 +12,11 @@ io.github.shunshun94.trpg.logEditor.menu.saveMenu.generateDom = (target) => {
 	>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} html"
         >HTMLとして出力（再編集可能）</button><br/><br/>
-        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} css"
+        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save disableInConvert" value="${target} css"
         >CSSとして出力</button><br/><br/>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} htmlCss"
         >CSSつきHTMLとして出力</button><br/><br/>
-        <span><button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} ytchat"
+        <span><button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save disableInConvert" value="${target} ytchat"
         >ゆとチャットっぽいHTMLとして出力</button><br/><br/></span>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} hameln"
         >ハーメルン用テキストファイルとして出力</button><br/><br/>
@@ -24,16 +24,16 @@ io.github.shunshun94.trpg.logEditor.menu.saveMenu.generateDom = (target) => {
         >Pixiv小説用テキストファイルとして出力</button><br/><br/>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save" value="${target} commonText"
         >汎用テキストファイルとして出力</button><br/><br/>
-        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save " value="${target} operationTable"
+        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save disableInConvert " value="${target} operationTable"
         >分析・再利用用 HTML として出力（SW2.5とCoCにのみ対応 / フォーマットは今後変更の可能性あり）</button><br/><br/>
-        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save " value="${target} operationJson"
+        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save  disableInConvert" value="${target} operationJson"
         >分析・再利用用 JSON として出力（SW2.5とCoCにのみ対応 / フォーマットは今後変更の可能性あり）</button><br/><br/>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save " value="${target} UniCoeText"
         >ユニコエへの挿入用テキストファイルとして出力（SW2.5とCoCにのみ対応 / フォーマットは今後変更の可能性あり）</button><br/><br/>
         <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save " value="${target} YukkuriMovieMaker"
         >ゆっくりムービーメーカーの台本への挿入用テキストファイルとして出力（SW2.5とCoCにのみ対応 / フォーマットは今後変更の可能性あり）</button>
         <br/><br/>
-        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save experimentalfunction" value="${target} IdLogExporter"
+        <button class="${io.github.shunshun94.trpg.logEditor.CLASSES.SAVE_MENU_WINDOW}-save experimentalfunction  disableInConvert" value="${target} IdLogExporter"
         >ID のある要素の一覧を json 形式で出力</button>
 	</div>`;
 };
