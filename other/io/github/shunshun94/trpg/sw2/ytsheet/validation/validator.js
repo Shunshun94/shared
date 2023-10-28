d45c485593ea67e4218df2e98f224b1b9857e37f
Shunshun94 <shunshun9460@gmail.com>
2023-10-28 22:26:24 +0900

    modify function names

--- other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validator.js
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validator.js
@@ -6,18 +6,20 @@ io.github.shunshun94.trpg.sw2 = io.github.shunshun94.trpg.sw2 || {};
 io.github.shunshun94.trpg.sw2.ytsheet = io.github.shunshun94.trpg.sw2.ytsheet || {};
 io.github.shunshun94.trpg.sw2.ytsheet.validation = io.github.shunshun94.trpg.sw2.ytsheet.validation || {};
 
-io.github.shunshun94.trpg.sw2.ytsheet.validation.isMatch = (json, conditions) => {
+io.github.shunshun94.trpg.sw2.ytsheet.validation.when = (json, conditions) => {
     if( conditions.and ) {
         let result = true;
         for(var key in conditions.and) {
-            result = result && io.github.shunshun94.trpg.sw2.ytsheet.validation.isMatch(json, {key: conditions[key]});
+            result = result && io.github.shunshun94.trpg.sw2.ytsheet.validation.when(json, {key: conditions[key]});
         }
     } else {
         const list = conditions.or || conditions;
         for(var key in list) {
             if( json[key] ) {
+                console.log(key, json[key]);
                 return true;
             }
         }
     }
+    return false;
 };
