a456119075740608c42a7a093ace8032f01106bb
Shunshun94 <shunshun9460@gmail.com>
2023-10-29 02:39:49 +0900

    add getKeyValues

--- other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validator.js
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validator.js
@@ -6,11 +6,11 @@ io.github.shunshun94.trpg.sw2 = io.github.shunshun94.trpg.sw2 || {};
 io.github.shunshun94.trpg.sw2.ytsheet = io.github.shunshun94.trpg.sw2.ytsheet || {};
 io.github.shunshun94.trpg.sw2.ytsheet.validation = io.github.shunshun94.trpg.sw2.ytsheet.validation || {};
 
-io.github.shunshun94.trpg.sw2.ytsheet.validation.when = (json, conditions) => {
+io.github.shunshun94.trpg.sw2.ytsheet.validation.isMatch = (json, conditions) => {
     if( conditions.and ) {
         let result = true;
         for(var key in conditions.and) {
-            result = result && io.github.shunshun94.trpg.sw2.ytsheet.validation.when(json, {key: conditions[key]});
+            result = result && io.github.shunshun94.trpg.sw2.ytsheet.validation.isMatch(json, {key: conditions[key]});
         }
     } else {
         const list = conditions.or || conditions;
@@ -22,4 +22,18 @@ io.github.shunshun94.trpg.sw2.ytsheet.validation.when = (json, conditions) => {
         }
     }
     return false;
+};
+
+io.github.shunshun94.trpg.sw2.ytsheet.validation.when = io.github.shunshun94.trpg.sw2.ytsheet.validation.isMatch;
+
+io.github.shunshun94.trpg.sw2.ytsheet.validation.getKeyValues = (json, key) => {
+    if(json[key]) {
+        return { key: json[key] };
+    }
+    const re = new RegExp(key);
+    const result = {};
+    for(var key in json) {
+        if(re.test(key)) { result[key] = json[key]; }
+    }
+    return result;
 };
