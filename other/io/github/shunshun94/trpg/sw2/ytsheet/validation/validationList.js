1cae2fdaa8e081f2f54a597170d73c62ee4192e0
Shunshun94 <shunshun9460@gmail.com>
2023-11-03 03:16:27 +0900

    add Fai / Mag

--- other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validationList.js
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/validation/validationList.js
@@ -35,5 +35,23 @@ io.github.shunshun94.trpg.sw2.ytsheet.validation.VALIDATION_LIST = [
             'race': {includes: 'センティアン'}
         },
         ifNot: 'プリースト技能による魔法を行使するには聖印を装備している必要があります（『1』198頁）'
+    }, {
+        level: 'error',
+        when: {
+            'lvMag': 1
+        },
+        expect: {
+            'accessory.*Name': {includes: 'マギスフィア'}
+        },
+        ifNot: 'マギテック技能による魔法を行使するにはマギスフィアを装備している必要があります（『1』198頁）'
+    }, {
+        level: 'error',
+        when: {
+            'lvFai': 1
+        },
+        expect: {
+            'accessory.*Name': {includes: '宝石'}
+        },
+        ifNot: 'フェアリーテイマー技能による魔法を行使するには対応する宝石を装備している必要があります（『2』112頁）'
     }
 ];
