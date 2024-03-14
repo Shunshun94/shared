db60214a576a2760180184934c2ac8f3a5083d73
Shunshun94 <shunshun9460@gmail.com>
2024-03-15 03:01:36 +0900

    fit for line input

--- jquery/io/github/shunshun94/trpg/logEditor/convertors/rawTextConvertor.js
+++ jquery/io/github/shunshun94/trpg/logEditor/convertors/rawTextConvertor.js
@@ -6,27 +6,33 @@ io.github.shunshun94.trpg.logEditor = io.github.shunshun94.trpg.logEditor || {};
 io.github.shunshun94.trpg.logEditor.convertors = io.github.shunshun94.trpg.logEditor.convertors || {};
 io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor = io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor ||{};
 
+io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.CONSTS = {
+	is2024Format: /^\s\[LINE\]\s/,
+	postSpliter: { old: ' ', in2024: '\t' },
+	LINE_Spliter: /^\d\d\d\d\.\d\d\.\d\d\s[日月火水木金土]曜日$|^\s*\[LINE\]\s*.*トーク履歴$|^\d\d\d\d\/\d\d?\/\d\d?\([日月火水木金土]\)$|^保存日時：\d\d\d\d\/\d?\d\/\d?\d\s+\d\d?:\d?\d$/gm
+};
+
 io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.dropEventToJson = (file) => {
 	return new Promise((resolve, reject)=>{
 		io.github.shunshun94.trpg.logEditor.convertors.ConvertorFactory.fileToText(file).then((rawText)=>{
-			console.log(rawText.split(/^\d\d\d\d\.\d\d\.\d\d\s[日月火水木金土]曜日|^\[LINE\]\s+.*トーク履歴$|^\d\d\d\d\/\d\d?\/\d\d?\([日月火水木金土]\)$/gm));
-			resolve(io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.lineToJson(rawText));
+			const version = io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.CONSTS.is2024Format.test(rawText) ? 'in2024' : 'old';
+			resolve(io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.lineToJson(rawText, io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.CONSTS.postSpliter[version]));
 		});
 	});
 };
 
-io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.lineToJson = (text) => {
+io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.lineToJson = (text, spliter) => {
 	const posts = [];
-	text.split(/^\d\d\d\d\.\d\d\.\d\d\s[日月火水木金土]曜日|^\[LINE\]\s+.*トーク履歴$|^\d\d\d\d\/\d\d?\/\d\d?\([日月火水木金土]\)$/gm).filter((d)=>{
-		return d;
-	}).map((d)=>{
+	text.split(io.github.shunshun94.trpg.logEditor.convertors.RawTextConvertor.CONSTS.LINE_Spliter).map((d)=>{
 		return d.trim();
+	}).filter((d)=>{
+		return d;
 	}).map((d)=>{
 		const lines = d.split('\n');
 		for(const line of lines) {
-			if(/^\d\d:\d\d/.test(line)) {
+			if(/^\d?\d:\d?\d/.test(line)) {
 				const result = {tag: 'p', id:'', class:'', style:''};
-				const lineParts = line.split(' ');
+				const lineParts = line.split(spliter);
 				result.name = lineParts[1];
 				result.content = lineParts.slice(2).join(' ');
 				result.tabName = '';
