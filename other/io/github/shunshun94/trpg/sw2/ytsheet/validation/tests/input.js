42bedda02b13416a2fe3fa695946d4d995bc1b81
Shunshun94 <shunshun9460@gmail.com>
2024-05-01 01:47:18 +0900

    add about scout tool as スカウトツール

--- other/io/github/shunshun94/trpg/sw2/ytsheet/validation/tests/input.js
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/validation/tests/input.js
@@ -1116,6 +1116,11 @@ io.github.shunshun94.trpg.sw2.ytsheet.validation.VALIDATION_TEST_LIST = {
             'items': 'スカウト用ツール',
             'expect': true
         }, {
+            'testName': 'スカウト技能でスカウト用ツールを持っている場合',
+            'lvSco': '1',
+            'items': 'スカウトツール',
+            'expect': true
+        }, {
             'testName': 'スカウト技能で精密ツールセットを持っている場合',
             'lvSco': '1',
             'items': '精密ツールセット',
