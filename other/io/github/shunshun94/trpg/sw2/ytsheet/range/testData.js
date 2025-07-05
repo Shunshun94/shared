7252adca82fc2d46d232320f075c7f991a7d8cc4
Shunshun94 <shunshun9460@gmail.com>
2025-07-05 17:14:06 +0900

    move testData to single file

---
+++ other/io/github/shunshun94/trpg/sw2/ytsheet/range/testData.js
@@ -0,0 +1,132 @@
+const testData = {
+    highLevelCharacterCreationTable: [
+    {
+        "level": 2,
+        "expTotal": 3000,
+        "expRest": 0,
+        "historyMoneyTotal": 1200,
+        "moneyTotal": 0,
+        "historyGrowTotal": 0
+    },
+    {
+        "level": 3,
+        "expTotal": 5500,
+        "expRest": 0,
+        "historyMoneyTotal": 2500,
+        "moneyTotal": 0,
+        "historyGrowTotal": 2
+    },
+    {
+        "level": 4,
+        "expTotal": 10000,
+        "expRest": 0,
+        "historyMoneyTotal": 6000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 5
+    },
+    {
+        "level": 5,
+        "expTotal": 16000,
+        "expRest": 0,
+        "historyMoneyTotal": 14000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 9
+    },
+    {
+        "level": 6,
+        "expTotal": 23000,
+        "expRest": 0,
+        "historyMoneyTotal": 24000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 13
+    },
+    {
+        "level": 7,
+        "expTotal": 30000,
+        "expRest": 0,
+        "historyMoneyTotal": 36000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 17
+    },
+    {
+        "level": 8,
+        "expTotal": 40000,
+        "expRest": 0,
+        "historyMoneyTotal": 54000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 22
+    },
+    {
+        "level": 9,
+        "expTotal": 50000,
+        "expRest": 0,
+        "historyMoneyTotal": 80000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 28
+    },
+    {
+        "level": 10,
+        "expTotal": 65000,
+        "expRest": 0,
+        "historyMoneyTotal": 120000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 34
+    },
+    {
+        "level": 11,
+        "expTotal": 85000,
+        "expRest": 0,
+        "historyMoneyTotal": 160000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 44
+    },
+    {
+        "level": 12,
+        "expTotal": 105000,
+        "expRest": 0,
+        "historyMoneyTotal": 210000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 54
+    },
+    {
+        "level": 13,
+        "expTotal": 135000,
+        "expRest": 0,
+        "historyMoneyTotal": 270000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 65
+    },
+    {
+        "level": 14,
+        "expTotal": 165000,
+        "expRest": 0,
+        "historyMoneyTotal": 350000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 77
+    },
+    {
+        "level": 15,
+        "expTotal": 200000,
+        "expRest": 0,
+        "historyMoneyTotal": 450000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 90
+    },
+    {
+        "level": 16,
+        "expTotal": 220000,
+        "expRest": 0,
+        "historyMoneyTotal": 500000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 100
+    },
+    {
+        "level": 17,
+        "expTotal": 250000,
+        "expRest": 0,
+        "historyMoneyTotal": 550000,
+        "moneyTotal": 0,
+        "historyGrowTotal": 110
+    }
+    ]
+}
