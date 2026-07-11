be1d6222cf87c4a4365975c8c3a12bea61ab9fb3
Shunshun94 <shunshun9460@gmail.com>
2026-07-11 22:59:27 +0900

    for edit page add-on

---
+++ other/io/github/shunshun94/ytsheet/addSkin/sw25_edit_character.js
@@ -0,0 +1,7 @@
+var io = io || {};
+io.github = io.github || {};
+io.github.shunshun94 = io.github.shunshun94 || {};
+io.github.shunshun94.ytsheet = io.github.shunshun94.ytsheet || {};
+io.github.shunshun94.ytsheet.addSkin = io.github.shunshun94.ytsheet.addSkin || {};
+io.github.shunshun94.ytsheet.addSkin.CharacterSheet = io.github.shunshun94.ytsheet.addSkin.CharacterSheet || {};
+console.log('io.github.shunshun94.ytsheet.addSkin.CharacterSheet');
